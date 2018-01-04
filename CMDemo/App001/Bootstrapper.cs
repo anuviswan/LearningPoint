7d7926cdd1218c606d8d18290849ff36dd94c012
Anu Viswan <anu@ecsolvo.com>
2018-01-04 14:03:30 +0530

    Added CMDemo

---
+++ CMDemo/App001/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using App001.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App001
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ CMDemo/App001/ViewModels/ShellViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App001.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        private string _FirstName = "Jia Anu";
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                _FirstName = value;
+                NotifyOfPropertyChange(nameof(FirstName));
+            }
+        }
+
+    }
+}
