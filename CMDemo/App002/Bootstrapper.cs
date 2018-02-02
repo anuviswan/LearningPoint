c5a02933efba0299fd8df858512258c7d89c7f82
Anu Viswan <anu@ecsolvo.com>
2018-02-02 21:17:11 +0530

    Updated Code

---
+++ CMDemo/App002/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using App002.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App002
+{
+    public class Bootstrapper : BootstrapperBase 
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            this.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ CMDemo/App002/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App002.ViewModels
+{
+    public class MainViewModel:Conductor<object>
+    {
+
+    }
+}
