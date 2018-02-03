002e3c7d2c5a5f066f8bee4b8513e428ef0c78b4
Anu Viswan <anu@ecsolvo.com>
2018-02-03 08:43:23 +0530

    Setting up Project

---
+++ Stimulsoft/Stimulsoft002/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+using Stimulsoft002.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stimulsoft002.ViewModels
+{
+    [Export(typeof(IShellViewModel))]
+    public class ShellViewModel:Screen, IShellViewModel
+    {
+    }
+}
