7814cad609294ed73211301fef7a9188b795cef0
Anu Viswan <anu@ecsolvo.com>
2018-02-03 09:20:48 +0530

    Added Behavior and linked StiWpfDesignerControl

---
+++ Stimulsoft/Stimulsoft002/Behaviors/ReportBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Stimulsoft.Report.WpfDesign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Stimulsoft002.Behaviors
+{
+    public class ReportBehavior : Behavior<StiWpfDesignerControl>
+    {
+        public static readonly DependencyProperty ReportSourceProperty = DependencyProperty.RegisterAttached("ReportSource", typeof(object), typeof(ReportBehavior), new PropertyMetadata(ReportSourceChanged));
+
+        private static void ReportSourceChanged(DependencyObject DependencyObject, DependencyPropertyChangedEventArgs PropertyChangedEvent)
+        {
+            var stidesigner = DependencyObject as StiWpfDesignerControl;
+
+            if (stidesigner != null)
+                stidesigner.Report = PropertyChangedEvent.NewValue as Stimulsoft.Report.StiReport;
+        }
+
+        public static void SetReportSource(DependencyObject target, object value)
+        {
+            target.SetValue(ReportSourceProperty, value);
+        }
+
+        public static object GetReportSource(DependencyObject target)
+        {
+            return target.GetValue(ReportSourceProperty);
+        }
+    }
+}
--- Stimulsoft/Stimulsoft002/ViewModels/ShellViewModel.cs
+++ Stimulsoft/Stimulsoft002/ViewModels/ShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using Stimulsoft002.Contracts;
+using Stimulsoft002.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
@@ -12,5 +13,35 @@ namespace Stimulsoft002.ViewModels
     [Export(typeof(IShellViewModel))]
     public class ShellViewModel:Screen, IShellViewModel
     {
+         #region Private Variables
+        private IList<ProductModel> _ProductCollection;
+        #endregion
+
+        #region Constructor
+        public ShellViewModel()
+        {
+            _ProductCollection = new List<ProductModel>()
+            {
+                new ProductModel() { Name = "Product 1", Price = 100 },
+                new ProductModel() { Name = "Product 2", Price = 200 }
+            };
+
+            ActiveReport = new Stimulsoft.Report.StiReport();
+            ActiveReport.RegBusinessObject("Products", _ProductCollection);
+            ActiveReport.Dictionary.Synchronize();
+        }
+        #endregion
+
+        private Stimulsoft.Report.StiReport _ActiveReport;
+
+        public Stimulsoft.Report.StiReport ActiveReport
+        {
+            get { return _ActiveReport; }
+            set
+            {
+                _ActiveReport = value;
+                this.NotifyOfPropertyChange(nameof(ActiveReport));
+            }
+        }
     }
 }
