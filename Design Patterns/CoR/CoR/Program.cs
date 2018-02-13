d337304c68c661e8cc49bb02d910f3a21d5006e7
Anu Viswan <anu@ecsolvo.com>
2018-02-13 14:42:58 +0530

    Added CoR

--- Design Patterns/CoR/CoR/Program.cs
+++ Design Patterns/CoR/CoR/Program.cs
@@ -10,6 +10,21 @@ namespace CoR
     {
         static void Main(string[] args)
         {
+            IWaterfall Handler1 = new Requirements();
+            IWaterfall Handler2 = new Design();
+            IWaterfall Handler3 = new Implementation();
+            IWaterfall Handler4 = new Verification();
+            IWaterfall Handler5 = new Deployment();
+            IWaterfall Handler6 = new Maintenance();
+
+            Handler1.NextStage = Handler2;
+            Handler2.NextStage = Handler3;
+            Handler3.NextStage = Handler4;
+            Handler4.NextStage = Handler5;
+            Handler5.NextStage = Handler6;
+
+            Handler1.Execute();
+            Console.ReadLine();
         }
     }
 }
---
+++ Design Patterns/CoR/CoR/Waterfall.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoR
+{
+    internal class Requirements : IWaterfall
+    {
+        public IWaterfall NextStage {get;set;}
+
+        public void Execute()
+        {
+            Console.WriteLine(nameof(Requirements));
+
+            if (NextStage is IWaterfall)
+                NextStage.Execute();
+        }
+    }
+
+    internal class Design : IWaterfall
+    {
+        public IWaterfall NextStage { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine(nameof(Design));
+
+            if (NextStage is IWaterfall)
+                NextStage.Execute();
+        }
+    }
+
+    internal class Implementation : IWaterfall
+    {
+        public IWaterfall NextStage { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine(nameof(Implementation));
+
+            if (NextStage is IWaterfall)
+                NextStage.Execute();
+        }
+    }
+
+    internal class Verification : IWaterfall
+    {
+        public IWaterfall NextStage { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine(nameof(Verification));
+
+            if (NextStage is IWaterfall)
+                NextStage.Execute();
+        }
+    }
+
+    internal class Deployment : IWaterfall
+    {
+        public IWaterfall NextStage { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine(nameof(Deployment));
+
+            if (NextStage is IWaterfall)
+                NextStage.Execute();
+        }
+    }
+
+    internal class Maintenance : IWaterfall
+    {
+        public IWaterfall NextStage { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine(nameof(Maintenance));
+
+            if (NextStage is IWaterfall)
+                NextStage.Execute();
+        }
+    }
+}
