45a9ee017f2495c03d0e28ab9d46ceb1a52c0cf7
Anu Viswan <anu.viswan@gmail.com>
2018-04-11 22:01:24 +0530

    Added Code Benchmarking DateTime

---
+++ FeatureExplore/BenchmarkDateTime/DateTimeMethods.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkDateTime
+{
+    public class DateTimeMethods
+    {
+        private DateTime testData = DateTime.Now;
+
+        [Benchmark]
+        public DateTime FloorWithNewOperator() => new DateTime(testData.Year, testData.Month, testData.Day, testData.Hour, 0, 0);
+
+        [Benchmark]
+        public DateTime FloorWithAddOperator() => testData.AddMinutes(-testData.Minute).AddSeconds(-testData.Second);
+
+        [Benchmark]
+        public DateTime FloorWithTicks()
+        {
+            var originalTicks = testData.Ticks;
+            var hoursSinceEpoch = originalTicks / TimeSpan.TicksPerHour;
+            var newTicks = hoursSinceEpoch * TimeSpan.TicksPerHour;
+            return new DateTime(newTicks);
+
+        }
+    }
+}
---
+++ FeatureExplore/BenchmarkDateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkDotNet.Running.BenchmarkRunner.Run<DateTimeMethods>();
+        }
+    }
+
+
+}
