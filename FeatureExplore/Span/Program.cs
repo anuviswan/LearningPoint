95dc14e81123c3f5f9651ceb7b12a184d7a1ebe5
Anu Viswan <anu.viswan@gmail.com>
2018-04-21 21:47:12 +0530

    Type Conversion using Span

--- FeatureExplore/Span/Program.cs
+++ FeatureExplore/Span/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,16 @@ namespace Span
         static void Main()
         {
             // Execute the Benchmark 
-            BenchmarkRunner.Run<BenchmarkDemo>();
+            ExecuteTypeCast();
+            ExecuteBenchmark();
+
         }
+
+        [Conditional("RELEASE")]
+        static void ExecuteBenchmark() => BenchmarkRunner.Run<BenchmarkDemo>();
+
+        [Conditional("DEBUG")]
+        static void ExecuteTypeCast() => new TypeCast().TypeCastPrimitiveTypes();
+
     }
 }
---
+++ FeatureExplore/Span/TypeCast.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Span
+{
+    public class TypeCast
+    {
+        public void TypeCastPrimitiveTypes()
+        {
+            long originalPrimitiveType = long.MaxValue;
+            Span<byte> byteSpan = BitConverter.GetBytes(originalPrimitiveType);
+            Span<int> intSpan = byteSpan.NonPortableCast<byte, int>();
+            int castPrimitiveType = intSpan[0];
+            Console.WriteLine($"Primitive Test : OriginalValue (long) = {originalPrimitiveType}, Cast Value (int) = {castPrimitiveType}");
+
+            var structInstance = new ValueStruct(25, 01);
+            byteSpan = BitConverter.GetBytes(structInstance.Property1);
+            intSpan = byteSpan.NonPortableCast<byte, int>();
+            int castStructType = intSpan[0];
+            Console.WriteLine($"Struct Test : OriginalValue (long) = {structInstance.Property1}, Cast Value (int) = {castStructType}");
+
+            Console.ReadLine();
+        }
+
+        struct ValueStruct
+        {
+            public long Property1 { get; set; }
+            public int Property2 { get; set; }
+
+            public ValueStruct(long property1,int property2)
+            {
+                Property1 = property1;
+                Property2 = property2;
+            }
+        }
+    }
+}
