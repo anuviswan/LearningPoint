6ca44ac98ede0cf018fff7490de621932b69ab7e
Anu Viswan <anu.viswan@gmail.com>
2018-05-19 14:07:26 +0530

    added Console Host for service

---
+++ WCF/WCF002_ConsoleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF002_ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(WCF001_HelloWorld.HelloWorldService)))
+                host.Open();
+
+            Console.WriteLine("Host Started");
+            Console.ReadLine();
+        }
+    }
+}
