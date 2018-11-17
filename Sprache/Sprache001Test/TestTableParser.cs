108de82ce2be859d610f89a9fbc67b785ba9cebf
Anu Viswan <anu.viswan@gmail.com>
2018-11-17 13:55:57 +0530

    Test case for alphanumeric headers

--- Sprache/Sprache001Test/TestTableParser.cs
+++ Sprache/Sprache001Test/TestTableParser.cs
@@ -9,17 +9,33 @@ namespace Sprache001Test
     [TestClass]
     public class TestTableParser
     {
-        private string _content = @"
+        private string _contentWithStringHeaders = @"
 wt      D       FV      FD
 34.4   34.11    23.1    0.11
 24.4   32.11    23    2.11
 ";
- 
+
+        private string _contentWithAlphanumericHeaders = @"
+wt      D2       F1V      FD
+34.4   34.11    23.1    0.11
+24.4   32.11    23    2.11
+";
+
         [TestMethod]
-        public void EnsureHeadersAreParsed()
+        public void EnsureStringHeadersAreParsed()
         {
             var inputHeaders = new List<string>(new []{ "wt", "D", "FV", "FD" });
-            var headers = TableParser.Headers.Parse(_content);
+            var headers = TableParser.Headers.Parse(_contentWithStringHeaders);
+            Assert.AreEqual(4, headers.Count);
+            CollectionAssert.AreEqual(inputHeaders, headers);
+        }
+
+
+        [TestMethod]
+        public void EnsureAlphaNumericHeadersAreParsed()
+        {
+            var inputHeaders = new List<string>(new[] { "wt", "D2", "F1V", "FD" });
+            var headers = TableParser.Headers.Parse(_contentWithAlphanumericHeaders);
             Assert.AreEqual(4, headers.Count);
             CollectionAssert.AreEqual(inputHeaders, headers);
         }
