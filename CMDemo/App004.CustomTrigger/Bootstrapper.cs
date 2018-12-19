321c176a4215baa5d10f34bbe847790dc05a5b4f
Anu Viswan <anu.viswan@gmail.com>
2018-12-19 22:27:16 +0530

    Added Gesture Demo Project

---
+++ CMDemo/App004.CustomTrigger/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using App004.CustomTrigger.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App004.CustomTrigger
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        #endregion
+    }
+}
---
+++ CMDemo/App004.CustomTrigger/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App004.CustomTrigger.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        public ShellViewModel() => Count = 0;
+        public int Count { get; set; }
+    }
+}
