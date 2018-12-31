586b64c9f3ed8e336bc144e53872396925e1ec9f
Anu Viswan <anu.viswan@gmail.com>
2018-12-31 08:03:04 +0530

    Filtered files NOT accessed in last 7 days

--- Python/Scripting/desktop-cleaner.py
+++ Python/Scripting/desktop-cleaner.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 
-# get all files from directory
+# Get all files from directory, along with Last Accessed TimeStamp
 def GetListOfFilesFromDirectory(dirpath):
     _files = os.listdir(dirpath)
     _returnList = []
@@ -10,10 +10,29 @@ def GetListOfFilesFromDirectory(dirpath):
         _returnList.append((_fullPath,os.stat(_fullPath).st_atime))
     return _returnList
 
-def RetrieveRecords():
+# Filter Files = Not accessed for past 7 days
+def FilterRecords(files):
+    
+    _weekAgo = datetime.date.today() - datetime.timedelta(days=7)
+
+    _returnList=[]
+    for file in sorted(files,key=lambda x:x[1]):
+        lastAccessedDate = datetime.datetime.fromtimestamp(file[1])
+        if(_weekAgo > lastAccessedDate.date()):
+            _returnList.append(file[0])
+    return _returnList
+
+# Print Files
+def PrintList(files):
+    for file in files:
+        print(file)
+    
+# Archive Desktop
+def ArchiveDesktop():
     _desktopPath = os.path.expanduser("~\\Desktop")
     files = GetListOfFilesFromDirectory(_desktopPath)
-    for file in sorted(files,key=lambda x:x[1]):
-        print("%s , Last Accessed : %s" %(file[0],datetime.datetime.fromtimestamp(file[1])))
+    filterfiles = FilterRecords(files)
+    PrintList(filterfiles)
 
-RetrieveRecords()
+
+ArchiveDesktop()
