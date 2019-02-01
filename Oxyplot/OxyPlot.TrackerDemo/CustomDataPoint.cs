01491b72560587d15caf78678aac65f7efa96210
Anu Viswan <anu.viswan@gmail.com>
2019-02-01 06:15:34 +0530

    Added Example for OxyPlot Tracker

---
+++ Oxyplot/OxyPlot.TrackerDemo/CustomDataPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlot.TrackerDemo
+{
+    public class CustomDataPoint : IDataPointProvider
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Description { get; set; }
+        public DataPoint GetDataPoint() => new DataPoint(X, Y);
+
+        public CustomDataPoint(double x, double y, string description)
+        {
+            X = x;
+            Y = y;
+            Description = description;
+        }
+    }
+}
---
+++ Oxyplot/OxyPlot.TrackerDemo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using OxyPlot.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlot.TrackerDemo.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        public ShellViewModel()
+        {
+            var model = new PlotModel { Title = "Example Tracker" };
+            var points  = new List<CustomDataPoint>
+                              {
+                                  new CustomDataPoint(0, 4,"point 1"),
+                                  new CustomDataPoint(10, 13,"point 2"),
+                                  new CustomDataPoint(20, 15,"point 3"),
+                                  new CustomDataPoint(30, 16,"point 4"),
+                                  new CustomDataPoint(40, 12,"point 5"),
+                                  new CustomDataPoint(50, 12,"point 6")
+                              };
+            var seriesVisible = new OxyPlot.Series.LineSeries
+            {
+                Color = OxyColors.Blue,
+                MarkerFill = OxyColors.Red,
+                ItemsSource = points,
+                TrackerFormatString = "X={2},\nY={4},\nAdditionalInfo={Description}",
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 5
+                
+            };
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = Axes.AxisPosition.Bottom
+            });
+
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = Axes.AxisPosition.Left
+            });
+            model.Series.Add(seriesVisible);
+            MyModel = model;
+            
+
+            CustomPlotController = new PlotController();
+            CustomPlotController.UnbindAll();
+            CustomPlotController.BindTouchDown(PlotCommands.PointsOnlyTrackTouch);
+            MyModel.InvalidatePlot(true);
+        }
+
+        public PlotModel MyModel { get; set; }
+
+        public PlotController CustomPlotController { get; set; }
+    }
+}
