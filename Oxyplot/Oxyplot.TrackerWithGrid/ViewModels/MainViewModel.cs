937b515462ef0afa57f31094035cd1eb8abaf0e0
Anu Viswan <anu.viswan@gmail.com>
2019-04-07 16:18:49 +0530

    Display Datagrid

--- Oxyplot/Oxyplot.TrackerWithGrid/ViewModels/MainViewModel.cs
+++ Oxyplot/Oxyplot.TrackerWithGrid/ViewModels/MainViewModel.cs
@@ -6,17 +6,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Oxyplot.TrackerWithGrid;
+using OxyPlot;
+
 namespace Oxyplot.TrackerWithGrid.ViewModels
 {
     public class MainViewModel:Screen
     {
-        public IObservableCollection<Fruit> Fruits = new BindableCollection<Fruit>();
+        public IObservableCollection<Fruit> Fruits { get; set; } = new BindableCollection<Fruit>();
 
         public MainViewModel()
         {
             GenerateFruitCollection();
             NotifyOfPropertyChange(nameof(Fruits));
         }
+
+        public PlotModel DataPlotModel { get; set; }
 
         private void GenerateFruitCollection()
         {
