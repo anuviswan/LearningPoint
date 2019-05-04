453f7caf28dd9ff66ec43224b066817aff909e69
Anu Viswan <anu.viswan@gmail.com>
2019-05-04 20:50:29 +0530

    Implementing example of Composite Pattern

---
+++ Design Patterns/Composite/DirectoryObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Composite
+{
+    public class DirectoryObject : IMetaInfo
+    {
+        private string _directoryName;
+        private List<IMetaInfo> _fileCollection;
+        public DirectoryObject(string directoryName)
+        {
+            _directoryName = directoryName;
+            _fileCollection = new List<IMetaInfo>();
+        }
+
+        public void Add(IMetaInfo file)
+        {
+            _fileCollection.Add(file);
+        }
+
+        public void Remove(IMetaInfo file)
+        {
+            _fileCollection.Remove(file);
+        }
+        public double GetSize()
+        {
+            var directorySize = 0d;
+            foreach (var file in _fileCollection)
+            {
+                directorySize += file.GetSize();
+            }
+            return directorySize;
+        }
+    }
+}
---
+++ Design Patterns/Composite/FileObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Composite
+{
+    public class FileObject : IMetaInfo
+    {
+        private string _fileName;
+        public FileObject(string fileName)
+        {
+            _fileName = fileName;
+        }
+        public double GetSize()
+        {
+            Console.WriteLine($"Calculating size of {_fileName}");
+            return 0;
+        }
+    }
+}
