a23f121ce98286137475e85ace61e88065d68204
Anu Viswan <anu.viswan@gmail.com>
2019-05-31 06:37:33 +0530

    Adding Unit Tests for Design Patterns

---
+++ Design Patterns/Singleton/MySingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class MySingleton
+    {
+        private static readonly Lazy<MySingleton> _instance = new Lazy<MySingleton>(() => new MySingleton());
+
+        public static MySingleton GetInstance()
+        {
+            return _instance.Value;
+        }
+
+        public int Value { get; set; }
+    }
+}
