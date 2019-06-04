19e933cae51897b9ab8a33111f13b57efacf5b64
Anu Viswan <anu.viswan@gmail.com>
2019-06-04 21:50:49 +0530

    Preparing Sprache Tutorial Data

---
+++ Sprache/Sprache.Tests/EmailParserTests.cs
@@ -0,0 +1,30 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Sprache.Tests
+{
+    [TestFixture]
+    public class EmailParserTests
+    {
+        [Test]
+        public IEnumerable<string> Parse(string inputString)
+        {
+            var parser = new EmailParser();
+            var result = parser.Parser(inputString);
+            return result;
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("this is a  test without email").Returns(Enumerable.Empty<string>());
+            }
+        }
+
+    }
+}
---
+++ Sprache/Sprache/IParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprache
+{
+    public interface IParser<T>
+    {
+        IEnumerable<T> Parser(string data);
+    }
+}
