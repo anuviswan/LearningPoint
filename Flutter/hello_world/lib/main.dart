fd6dbc6ce1644cb422325d80ce54dacd6c25b56b
Anu Viswan <anu.viswan@gmail.com>
2019-06-08 23:48:09 +0530

    Hellow World Working copy

--- Flutter/hello_world/lib/main.dart
+++ Flutter/hello_world/lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-    new Text("This is a Hello World from Flutter",textDirection: TextDirection.ltr));
+    new Text("This is a Hello World from Flutter",textDirection: TextDirection.rtl));
