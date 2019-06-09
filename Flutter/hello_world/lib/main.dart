d0f97f31dc62f00f258c42ab629854b76de1b9d5
Anu Viswan <anu.viswan@gmail.com>
2019-06-09 07:59:56 +0530

    Removed stateless project

--- Flutter/hello_world/lib/main.dart
+++ Flutter/hello_world/lib/main.dart
@@ -5,3 +5,25 @@ void main() => runApp(
     child: new Text("This is a Hello World from Flutter",textDirection: TextDirection.rtl),
   )
     );
+
+
+class myStatelessApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Stateless Hello World !',
+      home: new Scaffold(
+        body: new Container(
+          color: Colors.blue,
+          child: new Container(
+            color: Colors.white,
+            margin: EdgeInsets.all(30),
+            child: new Center(
+              child: new Text('This is hello world app',textDirection: TextDirection.ltr,),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
