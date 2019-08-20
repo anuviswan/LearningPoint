a04829bf74f83bb64dcec2d58edfc6c28bd91c93
Anu Viswan <anu.viswan@gmail.com>
2019-08-20 22:28:57 +0530

    Added Solid - LiskovSubstitution Samples

---
+++ Solid/LiskovSubstitution/Preconditions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LiskovSubstitution
+{
+    public class School
+    {
+        public virtual int GetStudentCount(int minAge)
+        {
+            if (minAge <= 0)
+                throw new ArgumentOutOfRangeException("Invalid Age");
+            return default; // For demo purpose
+        }
+    }
+
+    public class Kindergarden : School
+    {
+        public override int GetStudentCount(int minAge)
+        {
+            if (minAge <= 0)
+                throw new ArgumentOutOfRangeException("Invalid Age");
+
+            if (minAge >= 5)
+                throw new ArgumentOutOfRangeException("Invalid Age");
+            return default; // For demo purpose
+        }
+    }
+}
---
+++ Solid/LiskovSubstitutions.Tests/Preconditions.cs
@@ -0,0 +1,29 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
+using System.Collections;
+using LiskovSubstitution;
+using NUnit.Framework;
+
+namespace LiskovSubstitutions.Tests
+{
+    [TestFixture]
+    public class Preconditions
+    {
+        [Test]
+        [TestCaseSource(typeof(Preconditions), nameof(TestCases))]
+        public void GetStudentCount(School school,int age)
+        {
+            Assert.Catch<ArgumentOutOfRangeException>(()=>school.GetStudentCount(age));
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new School(), 7);
+                yield return new TestCaseData(new Kindergarden(), 7);
+            }
+        }
+    }
+}
