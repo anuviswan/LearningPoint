d82ba500244a948687dbe328d11aed2936c57829
Anu Viswan <anu.viswan@gmail.com>
2019-09-09 09:15:27 +0530

    Invariant example

---
+++ Solid/LiskovSubstitution/Invariants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution.Invariants
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student()
+        {
+
+        }
+        public Student(string name,int age)
+        {
+            if (Age < 5)
+                throw new Exception("Age should be higher than 5");
+
+            Name = name;
+            Age = age;
+        }
+    }
+
+    public class NurseryStudent : Student
+    {
+        public NurseryStudent(string name, int age)//:base(name,age)
+        {
+            Age = age;
+            Name = name;
+        }
+        
+    }
+}
---
+++ Solid/LiskovSubstitutions.Tests/Invariants.cs
@@ -0,0 +1,32 @@
+﻿using LiskovSubstitution.Invariants;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutions.Tests
+{
+    [TestFixture]
+    public class Invariants
+    {
+        [Test]
+        [TestCaseSource(typeof(Invariants), nameof(TestCases))]
+        public void TestInvariants(Type studentType,string name, int age)
+        {
+            var student = (Student)Activator.CreateInstance(studentType,name,age);
+            Assert.AreEqual(age, student.Age);
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(typeof(Student),"Jia", 3);
+                yield return new TestCaseData(typeof(NurseryStudent), "Jia", 3);
+            }
+        }
+    }
+}
