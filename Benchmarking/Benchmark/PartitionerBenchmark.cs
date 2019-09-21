a4d020204bcafd677563a51052758e2c7b586d45
Anu Viswan <anu.viswan@gmail.com>
2019-09-21 09:31:31 +0530

    Added Benchmark for Partitioner

---
+++ Benchmarking/Benchmark/PartitionerBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class PartitionerBenchmark
+    {
+        [Benchmark]
+        public void ParallelLoopWithoutPartioner()
+        {
+            var maxValue = 100000;
+            var sum = 0L;
+
+            Parallel.For(0, maxValue, (value) =>
+            {
+                Interlocked.Add(ref sum, value);
+            });
+        }
+
+        [Benchmark]
+        public void ParallelLoopWithPartioner()
+        {
+            var maxValue = 100000;
+            var sum = 0L;
+            var partioner = Partitioner.Create(0,maxValue);
+
+            Parallel.ForEach(partioner, range =>
+            {
+                var (minValueInRange, maxValueInRange) = range;
+                var subTotal = 0;
+                for (int value = minValueInRange; value < maxValueInRange; value++)
+                {
+                    subTotal += value;
+                }
+                Interlocked.Add(ref sum, subTotal);
+            });
+        }
+    }
+}
