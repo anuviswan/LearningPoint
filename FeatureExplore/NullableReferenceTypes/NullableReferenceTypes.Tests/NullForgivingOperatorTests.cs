2c10bd52414e71f8cea0ec0eb93423be783d654f
Anu Viswan <anu.viswan@gmail.com>
2019-12-06 06:49:08 +0530

    Adding Null Forgiving Tests

---
+++ FeatureExplore/NullableReferenceTypes/NullableReferenceTypes.Tests/NullForgivingOperatorTests.cs
@@ -0,0 +1,22 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NullableReferenceTypes.Tests
+{
+    [TestFixture]
+    public class NullForgivingOperatorTests
+    {
+        [Test]
+        
+        public void TestMethod()
+        {
+            var instance = new NullForgivingOperator(null);
+            Assert.IsInstanceOf<NullForgivingOperator>(instance);
+            Assert.IsNotNull(instance);
+        }
+    }
+}
---
+++ FeatureExplore/NullableReferenceTypes/NullableReferenceTypes/NullForgivingOperator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+#nullable enable
+    public class NullForgivingOperator
+    {
+
+        public NullForgivingOperator(string sampleProperty)
+        {
+            var Settings = new S();
+            var CC = new S();
+            var num = 2;
+            var (msg, isCC, upd) = num switch
+            {
+                1 =>  ("Use this mode when you are first learning the phrases and their meanings.", Settings.Cc == CC.H, false),
+                2 =>  ("Use this mode to help you memorize the phrases and their meanings.",Settings.Cc == CC.H,false),
+                3 =>  ("Use this mode to run a self marked test.",Settings.Cc == CC.H,true),
+               _ => default,
+            };
+        
+
+        SampleProperty = sampleProperty ?? throw new ArgumentNullException(nameof(sampleProperty)); ;
+        }
+
+        public string SampleProperty { get; set; }
+    }
+
+    public class S
+    {
+        public bool Cc { get; set; }
+        public bool H { get; set; }
+    }
+}
