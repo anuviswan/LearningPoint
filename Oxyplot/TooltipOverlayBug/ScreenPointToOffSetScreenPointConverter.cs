e7822552b4fdc0dd68505da9be8169a967cd8abf
Anu Viswan <anu.viswan@gmail.com>
2020-01-02 11:47:39 +0530

    Bug Demo

---
+++ Oxyplot/TooltipOverlayBug/ScreenPointToOffSetScreenPointConverter.cs
@@ -0,0 +1,29 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TooltipOverlayBug
+{
+    public class ScreenPointToOffSetScreenPointConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ScreenPoint screenPoint)
+            {
+                return new ScreenPoint(screenPoint.X, screenPoint.Y + 100);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // TODO: Implement
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Oxyplot/TooltipOverlayBug/ViewModels/ShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooltipOverlayBug.ViewModels
+{
+    public class ShellViewModel
+    {
+        public ShellViewModel()
+        {
+            var dataPoints = Enumerable.Range(1, 10).Select(x => new DataPoint(x, x * 10));
+            GraphModel = new PlotModel();
+            GraphModel.Title = "Sample Test";
+            GraphModel.Series.Add(new LineSeries
+            {
+                ItemsSource = dataPoints,
+                Color = OxyColors.Blue,
+                MarkerType = MarkerType.Square,
+                TrackerFormatString = $"{{ 0 }}\n\n\n\n\n\n\n{{ 1 }}:  {{2:0.###}}\n\n\n\n\n\n\n\n{{3}}: {{4:0.###}}"
+            }); ;
+            GraphModel.InvalidatePlot(true);
+        }
+
+        public PlotModel GraphModel { get; set; }
+    }
+}
