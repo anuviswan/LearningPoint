4d0c4b9040bb3b3ecc6bd2187c7d16f3011a00a8
Anu Viswan <anu.viswan@gmail.com>
2020-03-06 11:58:39 +0530

    Buttons

--- Elm/HelloWorld/src/Main.elm
+++ Elm/HelloWorld/src/Main.elm
@@ -1,8 +1,53 @@
 module Main exposing (main)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing(..)
+import Html.Events exposing (onClick)
 
-import Html exposing (Html, text)
+-- Main
 
-
-main : Html msg
 main =
-    text "Hello, World!"
+    Browser.sandbox {init=init,update=update,view=view}
+
+
+type alias Model = Int
+
+init:Model
+init = 
+    0
+
+-- Update
+type Msg = Increment
+           | Decrement
+           | Reset
+           | IncrementBy10
+           | DecrementBy10
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment  ->
+            model +1
+        Decrement ->
+            model - 1
+        Reset ->
+            0
+        IncrementBy10 ->
+            model + 10
+        DecrementBy10 ->
+            model - 10
+
+-- View
+view : Model -> Html Msg
+view model =
+    div[class "jumbotron"]
+    [
+        div[][h1[][text "Hello World"],h4[][text "From Elm"]],
+        button[onClick DecrementBy10][text "-10"],
+        button[onClick Decrement][text "-"],
+        div[][ text (String.fromInt model)],
+        button[onClick Increment][text "+"],
+        button[onClick IncrementBy10][text "+10"],
+        button[onClick Reset][text "Reset"]
+    ]
+
