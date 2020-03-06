e79fc233833ee8d794588440fa24c187d9e0f637
Anu Viswan <anu.viswan@gmail.com>
2020-03-06 21:28:54 +0530

    Working on Forms

--- Elm/Forms/src/Main.elm
+++ Elm/Forms/src/Main.elm
@@ -1,11 +1,11 @@
 module Main exposing (main)
 import Browser
 import Html exposing(..)
-import Html.Events exposing(onClick)
+import Html.Attributes exposing(..)
+import Html.Events exposing (onInput)
 
 main =
-    Browser.sandbox {init=init,update=update,model=model}
-
+    Browser.sandbox {init=init,update=update,view=view}
 
 type alias Model =
     {
@@ -20,7 +20,6 @@ init =
         username = ""
         ,password = ""
         ,confirmpassword = ""
-        
     }
 
 
@@ -30,5 +29,45 @@ type Msg = UsernameChanged String
     | ConfirmPasswordChanged String
 
 update : Msg -> Model -> Model
+update msg model=
+    case msg of
+        UsernameChanged newvalue ->
+            {model|username = newvalue}
+        PasswordChanged newvalue ->
+            {model|password = newvalue}
+        ConfirmPasswordChanged newvalue ->
+            {model|confirmpassword = newvalue}
+
+-- view
+
+view : Model -> Html Msg
+view model =
+    div[class "jumbotron"]
+    [
+        div[][
+            viewTitle "UserName"
+            ,viewInput "text" "User Name" model.username UsernameChanged
+        ]
+        ,div[][
+            viewTitle "Password"
+            ,viewInput "password" "Password" model.password PasswordChanged
+        ]
+        ,div[][
+            viewTitle "Confirm Password"
+            ,viewInput "password" "Confirm Password" model.password ConfirmPasswordChanged
+        ]
+        
+        
+        
+    ]
+        
+viewInput : String -> String -> String -> (String->msg) -> Html msg
+viewInput typ pholder val action =
+    input[type_ typ, placeholder pholder,value val,onInput action][]
+
+viewTitle : String -> Html msg
+viewTitle message =
+    text message
+
 
 
