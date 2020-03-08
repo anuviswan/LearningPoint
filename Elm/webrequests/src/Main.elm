160bf291ad80b8dc89063788930efc5868773305
Anu Viswan <anu.viswan@gmail.com>
2020-03-08 18:56:45 +0530

    formatting

--- Elm/webrequests/src/Main.elm
+++ Elm/webrequests/src/Main.elm
@@ -1,17 +1,31 @@
 module Main exposing (main)
+
 import Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
+
 -- Main
+
+
 main =
-    Browser.element {init = init, update = update, subscriptions = subscriptions,view = view}
+    Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
+
+
+
 -- Model
-type Model = Sucess String
+
+
+type Model
+    = Sucess String
     | Loading
     | Failure
 
-init : () -> (Model,Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
+
+
+
 -- View
 -- Update
