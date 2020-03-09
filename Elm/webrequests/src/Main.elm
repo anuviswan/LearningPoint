b3b04f785952022b58704d56e602ae178e2ed440
Anu Viswan <anu.viswan@gmail.com>
2020-03-09 06:30:17 +0530

    Http Requests

--- Elm/webrequests/src/Main.elm
+++ Elm/webrequests/src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (main)
 
-import Html
+import Browser
+import Html exposing (Html,text,pre)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-
+import Http
 
 
 -- Main
@@ -18,14 +19,55 @@ main =
 
 
 type Model
-    = Sucess String
+    = Success String
     | Loading
     | Failure
 
 
 init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = "https://elm-lang.org/assets/public-opinion.txt"
+        , expect = Http.expectString GotText
+        }
+    )
 
 
 
 -- View
 -- Update
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fulltext ->
+                    ( Success fulltext, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "I was unable to load your book."
+
+        Loading ->
+            text "Loading..."
+
+        Success fullText ->
+            pre [] [ text fullText ]
