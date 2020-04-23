0facef8feabd72d46dd525e22d58945972b6b22e
Anu Viswan <anu.viswan@gmail.com>
2020-04-23 05:55:15 +0530

    #20 Refactor

--- reactjs/photowall/src/index.js
+++ reactjs/photowall/src/index.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Main from './Components/Main'
 
-class List extends Component {
-  render() {
-    return (<ol>
-      {this.props.names.map((n, index) => <li key={index}>{n}</li>)}
-    </ol>);
-  }
-}
 
-class Title extends Component {
-  render() {
-    return (<h1>{this.props.title}</h1>);
-  }
-}
-
-class Main extends Component {
-  render() {
-    return (<div>
-      <Title title={"Names"} />
-      <List names={["jia", "anu", "anu"]} />
-      <List names={["sree", "jia", "anu"]} />
-    </div>)
-  }
-}
 ReactDOM.render(<Main />, document.getElementById('root'));
