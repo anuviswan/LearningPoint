7e977753a7cbc93d7e78a01702cf5917cefa3db7
Anu Viswan <anu.viswan@gmail.com>
2020-04-25 13:30:37 +0530

    Example for Stateless methods

--- reactjs/photowall/src/Components/Title.js
+++ reactjs/photowall/src/Components/Title.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Title extends Component {
-    render() {
-        return (<h1>{this.props.title}</h1>);
-    }
+// Example for Stateless functions
+function Title(props) {
+    return (<h1>{props.title}</h1>);
 }
 
 export default Title;
