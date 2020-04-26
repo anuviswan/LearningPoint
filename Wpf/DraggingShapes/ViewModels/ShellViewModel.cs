6b1b5e23d30a4dcedf849f213556794e0cc3ff23
Anu Viswan <anu.viswan@gmail.com>
2020-04-26 07:31:05 +0530

    Impllemented draggable feature

--- Wpf/DraggingShapes/ViewModels/ShellViewModel.cs
+++ Wpf/DraggingShapes/ViewModels/ShellViewModel.cs
@@ -4,13 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace DraggingShapes.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel:PropertyChangedBase
     {
         public double Left { get; set; } = 10;
         public double Top { get; set; } = 10;
 
+        public bool IsShapeCaptured { get; set; }
+
+        public void MouseDown()
+        {
+            IsShapeCaptured = true;
+            NotifyOfPropertyChange(nameof(IsShapeCaptured));
+        }
+
+        public void MouseMove()
+        {
+            if (!IsShapeCaptured) return;
+            NotifyOfPropertyChange(nameof(Left));
+            NotifyOfPropertyChange(nameof(Top));
+        }
+
+        public void MouseUp()
+        {
+            IsShapeCaptured = false;
+            NotifyOfPropertyChange(nameof(IsShapeCaptured));
+        }
     }
 }
