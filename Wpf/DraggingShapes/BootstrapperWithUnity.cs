3b6d58289cf7d143843f7da148b51d8e23b5dd29
Anu Viswan <anu.viswan@gmail.com>
2020-04-26 06:22:16 +0530

    Draggable Shapes

---
+++ Wpf/DraggingShapes/BootstrapperWithUnity.cs
@@ -0,0 +1,57 @@
+﻿using Caliburn.Micro;
+using DraggingShapes.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+using Unity.Lifetime;
+
+namespace DraggingShapes
+{
+    public class BootstrapperWithUnity : BootstrapperBase
+    {
+        private IUnityContainer _unityContainer;
+        #region Constructor
+        public BootstrapperWithUnity()
+        {
+            Initialize();
+        }
+        #endregion
+
+        #region Overrides
+        protected override void Configure()
+        {
+            _unityContainer = new UnityContainer();
+            _unityContainer.RegisterInstance<IWindowManager>(new WindowManager());
+            _unityContainer.RegisterInstance<IEventAggregator>(new EventAggregator(), new ContainerControlledLifetimeManager());
+
+            //View Models
+             _unityContainer.RegisterInstance<ShellViewModel>(new ShellViewModel());
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _unityContainer.BuildUp(instance);
+            base.BuildUp(instance);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return string.IsNullOrEmpty(key) ? _unityContainer.Resolve(service, key) : _unityContainer.Resolve(service);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _unityContainer.ResolveAll(service);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        #endregion
+    }
+}
