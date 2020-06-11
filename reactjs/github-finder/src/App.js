aa547e6d7f44911fccfa95bb681697f4879b0c69
Anu Viswan <anu.viswan@gmail.com>
2020-06-11 06:00:05 +0530

    Working in Navbar.js

--- reactjs/github-finder/src/App.js
+++ reactjs/github-finder/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+      </div>
+    );
+  }
 }
 
 export default App;
