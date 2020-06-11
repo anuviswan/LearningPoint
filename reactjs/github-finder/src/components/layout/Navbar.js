a2dd151299d1b5724a2d3b6adfc08a880ca18115
Anu Viswan <anu.viswan@gmail.com>
2020-06-11 06:00:32 +0530

    Working on Navbar.js

---
+++ reactjs/github-finder/src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Navbar extends Component {
+  static defaultProps = {
+    title: 'Default Finder',
+    icon: 'fab fa-github',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon}></i>
+          {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
