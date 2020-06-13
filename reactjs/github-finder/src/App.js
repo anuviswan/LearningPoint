f8dde0068bb9403deab01880fe60dd326d10d953
Anu Viswan <anu.viswan@gmail.com>
2020-06-13 20:07:47 +0530

    Working on user list

--- reactjs/github-finder/src/App.js
+++ reactjs/github-finder/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import { render } from '@testing-library/react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
-        <UserItem />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
--- reactjs/github-finder/src/components/users/UserItem.js
+++ reactjs/github-finder/src/components/users/UserItem.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  };
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
     return (
       <div className='card text-center'>
         <img
