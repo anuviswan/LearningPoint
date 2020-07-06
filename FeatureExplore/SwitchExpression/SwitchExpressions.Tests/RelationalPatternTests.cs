0882769051da1fdb36f474bf1723f541e2331855
Anu Viswan <anu.viswan@gmail.com>
2020-07-07 05:24:34 +0530

    Working on RelationalPattern

---
+++ FeatureExplore/SwitchExpression/SwitchExpressions.Tests/RelationalPatternTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SwitchExpressions.Tests
+{
+    public class RelationalPatternTests
+    {
+        private ISwitchExpression<Person> _evaluator;
+        public RelationalPatternTests()
+        {
+            _evaluator = new RelationalPattern();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestDataSource))]
+        public void PropertyPatternEvaluate(Person person, string expected)
+        {
+            var resultSwitchExpression = _evaluator.EvaluateSwitchExpression(person);
+            var resultSwitchStatement = _evaluator.EvaluateSwitchExpression(person);
+            Assert.Equal(expected, resultSwitchExpression);
+            Assert.Equal(resultSwitchStatement, resultSwitchExpression);
+        }
+
+
+        public static IEnumerable<object[]> TestDataSource => new List<object[]>
+        {
+            new object[]{new Person{FirstName="Anu", LastName="Viswan",Age=36}, "FirstName and Age Matched" },
+            new object[]{new Person { FirstName = "Manu", LastName = "Viswan", Age = 33 }, "LastName With Age Less than Matched" },
+            new object[]{new Person { FirstName = "John", LastName = "Doe", Age = 55 }, "LastName Matched" },
+            new object[] {new Person { FirstName = "Tony", LastName="Stark", Age = 40} , "Not Found" },
+            new object[] {null,"Input was null"}
+        };
+    }
+}
---
+++ FeatureExplore/SwitchExpression/SwitchExpressions/Patterns/RelationalPattern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchExpressions.Patterns
+{
+    public class RelationalPattern : ISwitchExpression<Person>
+    {
+        public string EvaluateSwitchExpression(Person criteria) => throw new NotImplementedException();
+    }
+}
