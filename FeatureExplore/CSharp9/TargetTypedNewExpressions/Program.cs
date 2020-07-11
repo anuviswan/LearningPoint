adff47d72fbbdd27dbaf88299663fc1b43b89608
Anu Viswan <anu.viswan@gmail.com>
2020-07-11 17:54:39 +0530

    Added code samples for TargetTypedNewExpressions

---
+++ FeatureExplore/CSharp9/TargetTypedNewExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TargetTypedNewExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new()
+            {
+                ["Name1"] = "Anu Viswan",
+                ["Name2"] = "Jia Anu"
+            };
+            Foo foo = new (){ FirstName = nameof(Foo.FirstName) };
+            foo.Get(new() { UserName = nameof(Bar.UserName) });
+        }
+    }
+
+    public class Foo
+    {
+        public string FirstName { get; set; }
+        public string Get(Bar bar) => bar.UserName;  
+    }
+
+    public class Bar
+    {
+        public string UserName { get; set; }
+    }
+}
