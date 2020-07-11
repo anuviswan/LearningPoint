ed5fe624c15a7944c463c3b0b43291ddbb47a963
Anu Viswan <anu.viswan@gmail.com>
2020-07-11 13:42:43 +0530

    CSharp 9 - Init  Properties

---
+++ FeatureExplore/CSharp9/InitProperties/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InitProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo
+            {
+                FirstName = "Anu",
+                LastName = "Viswan"
+            };
+        }
+    }
+
+    public class Foo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; init; }
+    }
+
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
