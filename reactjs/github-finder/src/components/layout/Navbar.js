6c5ba9d5308682d615fc8c976a9be8be53ab26b5
Anu Viswan <anu.viswan@gmail.com>
2020-07-17 06:04:55 +0530

    Fixed

--- reactjs/github-finder/src/components/layout/Navbar.js
+++ reactjs/github-finder/src/components/layout/Navbar.js
@@ -11,10 +11,10 @@ const Navbar = ({ icon, title }) => {
       </h1>
       <ul>
         <li>
-          <a href='/'>Home</a>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <a href='/about'>About</a>
+          <Link to='/about'>About</Link>
         </li>
       </ul>
     </nav>
