d0c0d4101f682604265122f3162a781a323ff5b8
Anu Viswan <anu.viswan@gmail.com>
2020-08-08 07:38:00 +0530

    Adding Events

--- vuejs/V001/v001/src/components/TodoItem.vue
+++ vuejs/V001/v001/src/components/TodoItem.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="todo-item">
-    <p>{{todo.title}}</p>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+      <button @click="$emit('del-todo',todo.id)" class="del">X</button>
+    </p>
   </div>
 </template>
 
@@ -8,6 +12,11 @@
 export default {
   name: "TodoItem",
   props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
 };
 </script>
 
