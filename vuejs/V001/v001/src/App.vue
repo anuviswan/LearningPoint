168968e3234d1544cb16afef40f1a9ce6a37b512
Anu Viswan <anu.viswan@gmail.com>
2020-08-08 15:31:24 +0530

    Integrating API calls

--- vuejs/V001/v001/src/App.vue
+++ vuejs/V001/v001/src/App.vue
@@ -10,28 +10,14 @@
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddToDo";
+import axios from "axios";
+
 export default {
   name: "App",
   components: { Todos, Header, AddTodo },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
@@ -43,6 +29,12 @@ export default {
       console.log(id);
       this.todos = this.todos.filter((todo) => todo.id != id);
     },
+  },
+  async created() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    this.todos = response.data;
   },
 };
 </script>
