bd22e92f75fdac81552178d99c2c02ffd912ce6a
Anu Viswan <anu.viswan@gmail.com>
2020-10-09 09:27:18 +0530

    Anonymous Types

---
+++ FeatureExplore/AnonymousTypes/AnonymousTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using BarAssembly;
+using System;
+using System.Net.Cache;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            foo.Print(new { Name = "Jia Anu", Age = 4 });
+
+            var bar= new Bar();
+            bar.Print(new { Name = "Jia Anu", Age = 4 });
+            bar.Print(new User{ Name = "Jia Anu", Age = 4 });
+            Console.ReadLine();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class Foo
+    {
+        public void Print<T>(T data)
+        {
+            Console.WriteLine($"Type : {typeof(T)}");
+            var properties = typeof(T).GetProperties();
+            foreach(var property in properties)
+            {
+                Console.WriteLine($"{property.Name} = { property.GetValue(data)}");
+            }
+        }
+    }
+}
---
+++ FeatureExplore/AnonymousTypes/BarAssembly/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BarAssembly
+{
+    public class Bar
+    {
+        public void Print<T>(T data)
+        {
+            Console.WriteLine($"Type : {typeof(T)}");
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.Name} = { property.GetValue(data)}");
+            }
+        }
+    }
+}
