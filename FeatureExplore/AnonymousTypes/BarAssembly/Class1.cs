65d8d175fa7d1b84164e75cc91a58cca1c0b4ed4
Anu Viswan <anu.viswan@gmail.com>
2020-10-10 11:40:15 +0530

    Anonymous Type Sample Project

--- FeatureExplore/AnonymousTypes/BarAssembly/Class1.cs
+++ FeatureExplore/AnonymousTypes/BarAssembly/Class1.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Data;
 
 namespace BarAssembly
 {
     public class Bar
     {
-        public void Print<T>(T data)
+        public void Print(object data)
         {
-            Console.WriteLine($"Type : {typeof(T)}");
-            var properties = typeof(T).GetProperties();
-            foreach (var property in properties)
-            {
-                Console.WriteLine($"{property.Name} = { property.GetValue(data)}");
-            }
+            var typeSchema = new { UserName = string.Empty, Age = default(int), JoiningDate = default(DateTime) };
+            var castInstance = Cast(typeSchema, data);
+            Console.WriteLine(data.GetType() == castInstance.GetType());
+        }
+        private static T Cast<T>(T typeHolder, Object x) where T : class
+        {
+            return (T)x;
         }
     }
 }
