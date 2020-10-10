c7e8941b1e9330a41d82eceb9a38c5f62f6e802a
Anu Viswan <anu.viswan@gmail.com>
2020-10-10 11:39:57 +0530

    Anonymous Types

--- FeatureExplore/AnonymousTypes/AnonymousTypes/Program.cs
+++ FeatureExplore/AnonymousTypes/AnonymousTypes/Program.cs
@@ -1,30 +1,78 @@
 ﻿using BarAssembly;
 using System;
 using System.Net.Cache;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
+[assembly: InternalsVisibleTo("BarAssembly")]
 namespace AnonymousTypes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var foo = new Foo();
-            foo.Print(new { Name = "Jia Anu", Age = 4 });
+            var user = new
+            {
+                UserName = "Anu Viswan",
+                Age = 36,
+                JoiningDate = new DateTime(2020,10,10),
+            };
 
-            var bar= new Bar();
-            bar.Print(new { Name = "Jia Anu", Age = 4 });
-            bar.Print(new User{ Name = "Jia Anu", Age = 4 });
-            Console.ReadLine();
+            var anotherInstance = new
+            {
+                UserName = "Anu Viswan",
+                Age = 36,
+                JoiningDate = new DateTime(2020, 10, 10),
+            };
+
+            Console.WriteLine($"user & anotherInstance - Equality :{user.Equals(anotherInstance)}"); // True
+
+            Console.WriteLine($"user & anotherInstance - Type Equality :{user.GetType() == anotherInstance.GetType()}"); // True
+
+            var differentInstance = new
+            {
+                UserName = "Anu Viswan",
+                JoiningDate = new DateTime(2020, 10, 10),
+                Age = 36,
+            };
+
+            Console.WriteLine($"user & differentInstance - Equality :{user.GetType() == differentInstance.GetType()}"); // False
+
+
+            // user.Age = 37;  // This line would throw error as anonymous types has read-only properties
+
+            user = new      // Recreate object for reassigning properties
+            {
+                UserName = user.UserName,
+                Age = 37,
+                JoiningDate = user.JoiningDate,
+            };
+
+            Console.WriteLine($"Anonymous Type is derieved from Object : {user is object}");
+
+            var foo = new Foo();
+            foo.Print(user);
+            foo.Print((object)user);
+
+            var bar = new Bar();
+            bar.Print(user);
+
         }
     }
 
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
+   
     public class Foo
     {
+        public void Print(object data)
+        {
+            var typeSchema = new { UserName = string.Empty, Age = default(int), JoiningDate = default(DateTime) };
+            var castInstance = Cast(typeSchema, data);
+        }
+
+        private static T Cast<T>(T typeHolder, Object x) where T : class
+        {
+            return (T)x;
+        }
         public void Print<T>(T data)
         {
             Console.WriteLine($"Type : {typeof(T)}");
