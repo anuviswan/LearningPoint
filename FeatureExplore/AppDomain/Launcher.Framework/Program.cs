661c1ca65a60449bb6a9737206d5313756653414
anu.viswan@gmail.com <njunni@1320>
2020-12-10 05:10:56 +0530

    Adding sample project for launching cefsharp browser build application in another app domain

---
+++ FeatureExplore/AppDomain/Launcher.Framework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Launcher.Framework
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Parent App domain : {Thread.GetDomain().FriendlyName}");
+            Console.WriteLine("Creating child app domain....");
+            var childDomain = AppDomain.CreateDomain("ChildDomain");
+            Console.WriteLine($"Child App Domain : {childDomain.FriendlyName}");
+            Console.WriteLine($"Launching application in child app domain..");
+            childDomain.ExecuteAssemblyByName("MockApp");
+            Console.ReadLine();
+        }
+    }
+}
