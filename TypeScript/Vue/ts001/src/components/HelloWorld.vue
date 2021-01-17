6f4c4790ecd655b289f956416285c63641604b75
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-01-18 04:19:47 +0530

    base skeleton app

---
+++ TypeScript/Vue/ts001/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <div>
+      <b>{{ helloMessage }}</b>
+      <b>{{ message }}</b>
+    </div>
+    <h4>{{ count }}</h4>
+    <button @click="incrementCounter">Increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  message = "Hello World";
+  count = 0;
+
+  get helloMessage(): string {
+    return this.message + " " + "Anu";
+  }
+
+  incrementCounter() {
+    return ++this.count;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
