b5c306a3b2c98e34b87381dc0535f8f1feb0516f
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-01-19 01:49:30 +0530

    updating demo

---
+++ TypeScript/Generic/demo/demo.js
@@ -0,0 +1,8 @@
+var myString = "Hello World";
+var myNumber = 1;
+var myBoolean = true;
+var strNumTuple = ['John Doe', 52];
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(strNumTuple);
---
+++ TypeScript/Generic/demo/demo.ts
@@ -0,0 +1,9 @@
+let myString:string = "Hello World";
+let myNumber:number = 1;
+let myBoolean:boolean = true;
+let strNumTuple:[string,number] = ['John Doe',52];
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(strNumTuple);
