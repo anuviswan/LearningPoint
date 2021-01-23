d93aed9b58bf014fd781787e5412d15bdfe09504
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-01-24 05:04:25 +0530

    Sample for ViewModel First Approach using Caliburn Micro

---
+++ CMDemo/App005.ViewModelFirst/ViewModels/ShellViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace App005.ViewModelFirst.ViewModels
+{
+    public class ShellViewModel:Conductor<object>
+    {
+        public UserProfileViewModel UserProfile { get; set; }
+
+        public IEnumerable<UserProfileViewModel> UserProfileCollection { get; set; }
+        public ShellViewModel()
+        {
+            UserProfile = IoC.Get<UserProfileViewModel>();
+            UserProfile.Name = "Anu Viswan";
+            UserProfile.Age = 37;
+
+            UserProfileCollection = Enumerable.Range(1, 10).Select(x => new UserProfileViewModel { Name = $"Sample Name {x}", Age = 37 + x });
+        }
+    }
+}
---
+++ CMDemo/App005.ViewModelFirst/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace App005.ViewModelFirst.ViewModels
+{
+    public class UserProfileViewModel:PropertyChangedBase
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
