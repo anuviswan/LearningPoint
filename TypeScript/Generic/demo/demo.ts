bdcf1f4278336ded52ea24d675a4bf75dd550e9a
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-01-26 04:38:22 +0530

    typescript types

--- TypeScript/Generic/demo/demo.ts
+++ TypeScript/Generic/demo/demo.ts
@@ -1,9 +1,20 @@
-let myString:string = "Hello World";
-let myNumber:number = 1;
-let myBoolean:boolean = true;
-let strNumTuple:[string,number] = ['John Doe',52];
+let myString;
+let myNumber;
+let myBoolean;
+let myArray:Array<string>;
+let strNumTuple:[string,number];
+let arrayOfTuple:Array<[string,number]>;
+
+myString = "Hello World";
+myNumber = 1;
+myBoolean = true;
+myArray = ['Sample','Array'];
+strNumTuple = ['John Doe',51];
+arrayOfTuple =  [['John Doe',52],['Jane Doe',50]];
 
 console.log(myString);
 console.log(myNumber);
 console.log(myBoolean);
+console.log(myArray);
 console.log(strNumTuple);
+console.log(arrayOfTuple);
