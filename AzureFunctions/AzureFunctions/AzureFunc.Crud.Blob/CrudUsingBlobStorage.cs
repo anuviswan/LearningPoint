00f6f72b7f87d3181b3ed66398d992dd8be2ca74
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-01-30 14:18:42 +0530

    Working on Create using Blob

--- AzureFunctions/AzureFunctions/AzureFunc.Crud.Blob/CrudUsingBlobStorage.cs
+++ AzureFunctions/AzureFunctions/AzureFunc.Crud.Blob/CrudUsingBlobStorage.cs
@@ -13,25 +13,45 @@ namespace AzureFunc.Crud.Blob
 {
     public static class CrudUsingBlobStorage
     {
-        [FunctionName("AddItem")]
-        public static async Task<IActionResult> AddItem(
+        [FunctionName("AddItemBlockBlob")]
+        public static async Task<IActionResult> AddItemBlockBlob(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-            [Blob("todos")] CloudBlobContainer blocContainer,
+            [Blob("todos")] CloudBlobContainer blobContainer,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Request Recieved");
 
-            await blocContainer.CreateIfNotExistsAsync();
-
+            await blobContainer.CreateIfNotExistsAsync();
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var data = JsonConvert.DeserializeObject<TodoDto>(requestBody);
 
-            data.Id = Guid.NewGuid().ToString();
+            if (string.IsNullOrEmpty(data.Id))
+            {
+                data.Id = Guid.NewGuid().ToString();
+            }
+            
             var serializedData = JsonConvert.SerializeObject(data);
 
-            var blob = blocContainer.GetBlockBlobReference($"{data.Id}.json");
+            var blob = blobContainer.GetBlockBlobReference($"{data.Id}.json");
             await blob.UploadTextAsync(serializedData);
             return new OkObjectResult($"Item added to blob with Id = {data.Id}");
+        }
+
+        [FunctionName("UploadFileBlockBlob")]
+        public static async Task<IActionResult> UploadFileBlockBlob(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [Blob("todos")] CloudBlobContainer blobContainer,
+            ILogger log
+            )
+        {
+            string key = req.Query["key"];
+            var data = req.Form.Files["file"];
+            using (var stream = data.OpenReadStream())
+            {
+                var blob = blobContainer.GetBlockBlobReference(key);
+                await blob.UploadFromStreamAsync(stream);
+            }
+            return new OkObjectResult($"Item added to blob with Id = {key}");
         }
     }
 
