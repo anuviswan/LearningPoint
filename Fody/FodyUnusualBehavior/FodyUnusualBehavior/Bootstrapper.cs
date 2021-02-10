776958f7ce6458b8fb82844d7ea2cbafc3a07268
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-02-11 05:03:32 +0530

    Added Example For Unusual behavior of Fody

---
+++ Fody/FodyUnusualBehavior/FodyUnusualBehavior/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace FodyUnusualBehavior
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // DisplayRootViewFor<IShell>();
+        }
+        #endregion
+    }
+}
---
+++ Fody/FodyUnusualBehavior/FodyUnusualBehavior/ViewModels/ShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Caliburn.Micro;
+
+namespace FodyUnusualBehavior.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        private Random _random;
+
+        public ShellViewModel()
+        {
+            _random = new Random();
+            //PropertyChanged += HandlePropertyChanged;
+            PropertyChanged += OnPropertyChanged;
+        }
+
+        //private void HandlePropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private void OnPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            // Do something here
+        }
+
+        public long RandomNumber { get; set; } = 0;
+
+        public void Randomize()
+        {
+            RandomNumber = _random.Next();
+        }
+    }
+}
