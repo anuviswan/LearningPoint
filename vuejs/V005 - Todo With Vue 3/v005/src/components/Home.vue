3fff4b3994d2d354cccd4ed0286fa448fa3677dc
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-02-17 12:09:37 +0530

    updating home

--- vuejs/V005 - Todo With Vue 3/v005/src/components/Home.vue
+++ vuejs/V005 - Todo With Vue 3/v005/src/components/Home.vue
@@ -1,10 +1,18 @@
 <template>
   <h1>Home</h1>
+  <ul>
+    <li v-for="task in currentTaskList" :key="task">{{ task }}</li>
+  </ul>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
+  setup() {
+    const currentTaskList = ref(["sdsd", "sdsdsd"]);
+    return { currentTaskList };
+  },
 };
 </script>
 
