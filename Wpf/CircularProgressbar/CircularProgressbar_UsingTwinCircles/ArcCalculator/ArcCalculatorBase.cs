ea5058b8ee35967ee830dce0fda99e92634b2231
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-03-09 05:02:33 +0530

    Working on second version of arc

---
+++ Wpf/CircularProgressbar/CircularProgressbar_UsingTwinCircles/ArcCalculator/ArcCalculatorBase.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CircularProgressbar_UsingTwinCircles.ArcCalculator
+{
+    public abstract class ArcCalculatorBase
+    {
+        protected const double ORIGIN = 50;
+        protected double _backgroundCircleThickness;
+        protected double _valueCircleThickness;
+        public ArcCalculatorBase(double backgroundCircleThickness, double valueCircleThickness)
+        {
+            _backgroundCircleThickness = backgroundCircleThickness;
+            _valueCircleThickness = valueCircleThickness;
+        }
+
+        public Size BackgroundCircleRadius { get; protected set; }
+        public Size ValueCircleRadius { get; protected set; }
+
+        public Point BackgroundCircleStartPosition { get; protected set; }
+        public Point BackgroundCircleEndPosition { get; protected set; }
+
+        public Point ValueCircleStartPosition { get; protected set; }
+        public Point ValueCircleEndPosition { get; protected set; }
+
+        public double ValueAngle { get; set; }
+
+        protected double GetAngleForValue(double minValue, double maxValue, double currentValue)
+        {
+            var percent = (currentValue - minValue) * 100 / (maxValue - minValue);
+            var valueInAngle = (percent / 100) * 360;
+            return valueInAngle;
+        }
+        public abstract void Calculate(double minValue, double maxValue, double currentValue);
+        protected Point GetPointForAngle(Size radiusInSize, double angle)
+        {
+            var radius = radiusInSize.Height;
+            angle = angle == 360 ? 359.99 : angle;
+            double angleInRadians = angle * Math.PI / 180;
+
+            double px = ORIGIN + (Math.Sin(angleInRadians) * radius);
+            double py = ORIGIN + (-Math.Cos(angleInRadians) * radius);
+
+            return new Point(px, py);
+        }
+    }
+
+    public class OutsetArcCalculator : ArcCalculatorBase
+    {
+        public OutsetArcCalculator(double backgroundCircleThickness, double valueCircleThickness) : base(backgroundCircleThickness, valueCircleThickness)
+        {
+
+        }
+
+        public override void Calculate(double minValue, double maxValue, double currentValue)
+        {
+            BackgroundCircleRadius = new Size(ORIGIN - _backgroundCircleThickness / 2, ORIGIN - _backgroundCircleThickness / 2);
+            ValueCircleRadius = new Size(ORIGIN - _valueCircleThickness / 2, ORIGIN - _valueCircleThickness / 2); ;
+
+            BackgroundCircleStartPosition = GetPointForAngle(BackgroundCircleRadius, 0);
+            BackgroundCircleEndPosition = GetPointForAngle(BackgroundCircleRadius, 360);
+
+            ValueAngle = GetAngleForValue(minValue, maxValue, currentValue);
+
+            ValueCircleStartPosition = GetPointForAngle(ValueCircleRadius, 0);
+            ValueCircleEndPosition = GetPointForAngle(ValueCircleRadius, ValueAngle);
+        }
+    }
+
+    public class CenteredArcCalculator : ArcCalculatorBase
+    {
+        public CenteredArcCalculator(double backgroundCircleThickness, double valueCircleThickness) : base(backgroundCircleThickness, valueCircleThickness)
+        {
+
+        }
+
+        public override void Calculate(double minValue, double maxValue, double currentValue)
+        {
+            var maxThickness = Math.Max(_backgroundCircleThickness, _valueCircleThickness);
+
+            BackgroundCircleRadius = new Size(ORIGIN - maxThickness / 2, ORIGIN - maxThickness / 2);
+            ValueCircleRadius = new Size(ORIGIN - maxThickness / 2, ORIGIN - maxThickness / 2); ;
+
+            BackgroundCircleStartPosition = GetPointForAngle(BackgroundCircleRadius, 0);
+            BackgroundCircleEndPosition = GetPointForAngle(BackgroundCircleRadius, 360);
+
+            ValueAngle = GetAngleForValue(minValue, maxValue, currentValue);
+
+            ValueCircleStartPosition = GetPointForAngle(ValueCircleRadius, 0);
+            ValueCircleEndPosition = GetPointForAngle(ValueCircleRadius, ValueAngle);
+        }
+    }
+
+    public class InsetArcCalculator : ArcCalculatorBase
+    {
+        public InsetArcCalculator(double backgroundCircleThickness, double valueCircleThickness) : base(backgroundCircleThickness, valueCircleThickness)
+        {
+
+        }
+
+        public override void Calculate(double minValue, double maxValue, double currentValue)
+        {
+            var maxThickness = Math.Max(_backgroundCircleThickness, _valueCircleThickness);
+
+            BackgroundCircleRadius = new Size((ORIGIN - maxThickness) + (_backgroundCircleThickness / 2), (ORIGIN - maxThickness) + (_backgroundCircleThickness / 2));
+            ValueCircleRadius = new Size((ORIGIN - maxThickness) + (_valueCircleThickness / 2), (ORIGIN - maxThickness) + (_valueCircleThickness / 2));
+
+            BackgroundCircleStartPosition = GetPointForAngle(BackgroundCircleRadius, 0);
+            BackgroundCircleEndPosition = GetPointForAngle(BackgroundCircleRadius, 360);
+
+            ValueAngle = GetAngleForValue(minValue, maxValue, currentValue);
+
+            ValueCircleStartPosition = GetPointForAngle(ValueCircleRadius, 0);
+            ValueCircleEndPosition = GetPointForAngle(ValueCircleRadius, ValueAngle);
+        }
+    }
+}
---
+++ Wpf/CircularProgressbar/CircularProgressbar_UsingTwinCircles/ViewModels/ShellViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace CircularProgressbar_UsingTwinCircles.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        public ShellViewModel()
+        {
+            BackgroundCircle.Angle = 360;
+            BackgroundCircle.PropertyChanged += OnCircleChanged;
+            ValueCircle.PropertyChanged += OnCircleChanged;
+            PropertyChanged += OnPropertyChanged; ;
+        }
+
+        private void OnPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(MaxValue):
+                case nameof(MinValue):
+                case nameof(CurrentValue):
+                case nameof(SelectedOverlayMode):
+                    RefreshControl();
+                    break;
+            }
+        }
+
+        private void OnCircleChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            RefreshControl();
+        }
+
+        protected override void OnViewAttached(object view, object context)
+        {
+            base.OnViewAttached(view, context);
+            RefreshControl();
+        }
+        private void RefreshControl()
+        {
+            ArcCalculatorBase arcCalculator;
+            switch (SelectedOverlayMode)
+            {
+                case OverlayMode.Centered:
+                    arcCalculator = new CenteredArcCalculator(BackgroundCircle.Thickness, ValueCircle.Thickness);
+                    break;
+                case OverlayMode.InnerCircle:
+                    arcCalculator = new InsetArcCalculator(BackgroundCircle.Thickness, ValueCircle.Thickness);
+                    break;
+                case OverlayMode.OuterCircle:
+                    arcCalculator = new OutsetArcCalculator(BackgroundCircle.Thickness, ValueCircle.Thickness);
+                    break;
+                default:
+                    arcCalculator = new OutsetArcCalculator(BackgroundCircle.Thickness, ValueCircle.Thickness);
+                    break;
+            }
+            arcCalculator.Calculate(MinValue, MaxValue, CurrentValue);
+
+            BackgroundCircle.Radius = arcCalculator.BackgroundCircleRadius;
+            BackgroundCircle.StartPosition = arcCalculator.BackgroundCircleStartPosition;
+            BackgroundCircle.EndPosition = arcCalculator.BackgroundCircleEndPosition;
+
+            ValueCircle.Radius = arcCalculator.ValueCircleRadius;
+            ValueCircle.StartPosition = arcCalculator.ValueCircleStartPosition;
+            ValueCircle.EndPosition = arcCalculator.ValueCircleEndPosition;
+            ValueCircle.Angle = arcCalculator.ValueAngle;
+        }
+
+        public ProgressArc BackgroundCircle { get; set; } = new ProgressArc();
+        public ProgressArc ValueCircle { get; set; } = new ProgressArc();
+        public double MinValue { get; set; } = 10;
+        public double MaxValue { get; set; } = 120;
+        public double CurrentValue { get; set; } = 60;
+
+        public OverlayMode SelectedOverlayMode { get; set; }
+    }
+}
