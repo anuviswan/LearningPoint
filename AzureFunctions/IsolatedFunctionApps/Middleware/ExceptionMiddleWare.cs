8181071afbcca9e6e73a99ad97c9a45e078cb06b
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-06-05 16:06:42 +0530

    Working on demo middleware

---
+++ AzureFunctions/IsolatedFunctionApps/Middleware/ExceptionMiddleWare.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+
+namespace IsolatedFunctionApps.Middleware
+{
+    public class ExceptionMiddleWare : IFunctionsWorkerMiddleware
+    {
+        public Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
