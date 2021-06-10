291a8dc4fe0428d02e3931da87ea92007bc2ec35
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-06-10 05:52:33 +0530

    Working on Multi Output Binding for isolated functions

---
+++ AzureFunctions/IsolatedFunctionApps/Dtos/CustomOutputType.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace IsolatedFunctionApps.Dtos
+{
+    public class CustomOutputType
+    {
+        [QueueOutput("SampleQueue")]
+        public string UserTask { get; set; }
+        public HttpResponseData UserResponse { get; set; }
+    }
+}
--- AzureFunctions/IsolatedFunctionApps/FunctionApps/StaticFunctions.cs
+++ AzureFunctions/IsolatedFunctionApps/FunctionApps/StaticFunctions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.Json;
+﻿using System.Net;
 using System.Threading.Tasks;
 using IsolatedFunctionApps.Dtos;
 using Microsoft.Azure.Functions.Worker;
@@ -32,5 +26,29 @@ namespace IsolatedFunctionApps.FunctionApps
             await response.WriteStringAsync($"Hello {data.Name}, Welcome to Isolated functions demo.");
             return response;
         }
+
+        [Function(nameof(MultiOutputSample))]
+        public static async Task<CustomOutputType> MultiOutputSample(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            // Use the FunctionContext.GetLogger to fetch instance of ILogger
+            var logger = executionContext.GetLogger(nameof(StaticFunctions));
+            logger.LogInformation("Started execution of function");
+
+            var data = await req.ReadFromJsonAsync<UserDto>();
+
+            // Should use HttpResponseData as response
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteStringAsync($"Hello {data.Name}, Welcome to Isolated functions demo.");
+
+
+            return new CustomOutputType
+            {
+                UserResponse = response,
+                UserTask = "Jia And Niana have logged in"
+            };
+        }
+
     }
 }
