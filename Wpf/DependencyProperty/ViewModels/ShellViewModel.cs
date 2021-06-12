2d77a5586cfc3609d7f7ffa2d3ec94b85fc13b9f
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-06-12 06:22:39 +0530

    Demo for Dependency Property Value Resolution

---
+++ Wpf/DependencyProperty/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace DependencyProperty.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+        }
+
+        public async Task DisplayValueResolution()
+        {
+            await ActivateItemAsync(new ValueResolutionViewModel());
+        }
+    }
+}
---
+++ Wpf/DependencyProperty/ViewModels/ValueResolutionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Caliburn.Micro;
+
+namespace DependencyProperty.ViewModels
+{
+    public class ValueResolutionViewModel:Screen
+    {
+        public string _buttonValue;
+        public string ButtonValue
+        {
+            get => _buttonValue;
+            set
+            {
+                _buttonValue = value;
+                NotifyOfPropertyChange(nameof(ButtonValue));
+            }
+        }
+
+        public void ClearValue(Button button)
+        {
+            button.ClearValue(Button.ContentProperty);
+        }
+    }
+}
