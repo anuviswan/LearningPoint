a291768e96b38a5971cd0103f1672a1941ee8cea
Anu Viswan <anu.viswan@gmail.com>
2021-06-19 07:57:40 +0530

    Added Comment

--- Design Patterns/DynamicMappingPattern/Program.cs
+++ Design Patterns/DynamicMappingPattern/Program.cs
@@ -7,9 +7,15 @@ namespace DynamicMappingPattern
 {
     class Program
     {
+        /// <summary>
+        ///  This is a INCOMPLETE CODE. This is NOT a pattern, but an attempt to create a better way for dynamic mapping
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Executing Client Code");
+
+            Console.WriteLine("This code is still incomplete. It is an attempted pattern for dynamic mapping. ");
 
             var productCollection = new List<IProduct>()
             {
