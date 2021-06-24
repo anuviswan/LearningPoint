4d300f0c9915b95ebf5e7566e58b1d8487daee0a
Anu Viswan <anu.viswan@gmail.com>
2021-06-24 08:06:14 +0530

    Update Program.cs
    Updated with more Linq Functionality

--- FeatureExplore/DotNet6/LinqEnhancements/Program.cs
+++ FeatureExplore/DotNet6/LinqEnhancements/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Console;
 
 namespace LinqEnhancements
 {
@@ -8,21 +9,43 @@ namespace LinqEnhancements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is demo code for enhancements in Linq for .Net 6");
-            var demoCollection = Enumerable.Range(1, 25).Select(x=> new User(x,$"User{x}"));
-            
-            Console.WriteLine("Complete Elements {0}", demoCollection.ToPrintString());
-            Console.WriteLine();
-            Console.WriteLine("Support for Range & Index");
-            Console.WriteLine("Enumerable.Take 10th to 10th from end: {0}", demoCollection.Take(10..^10).ToPrintString());
-            Console.WriteLine();
-            Console.WriteLine("Support for Default");
-            Console.WriteLine("Enumerable.FirstOrDefault(defaultValue) {0}:", demoCollection.FirstOrDefault(x => x.id > 50, new User(-1,"DefaultUser")));
-            Console.WriteLine("Enumerable.LastOrDefault(defaultValue) : {0}", demoCollection.LastOrDefault(x => x.id > 50, new User(-1, "DefaultUser")));
-            Console.WriteLine("Enumerable.SingleOrDefault(defaultValue) : {0}", demoCollection.SingleOrDefault(x => x.id > 50, new User(-1, "DefaultUser")));
+            WriteLine("This is demo code for enhancements in Linq for .Net 6");
+            var demoCollection = GetData(25); 
 
-            Console.ReadLine();
+            WriteLine("Complete Elements {0}", demoCollection.ToPrintString());
+
+            WriteLine("Support for TryGetNonEnumeratedCount");
+            WriteLine("Enumerable.FirstOrDefault(defaultValue) {0}", $"IsNonEnumeratedCount:{demoCollection.TryGetNonEnumeratedCount(out var enumCount)},Count:{enumCount}");
+            WriteLine("Enumerable.FirstOrDefault(defaultValue) {0}", $"IsNonEnumeratedCount:{demoCollection.ToList().TryGetNonEnumeratedCount(out var listCount)},Count:{listCount}");
+            WriteLine();
+
+            WriteLine("Support for Range & Index");
+            WriteLine("Enumerable.Take 10th to 10th from end: {0}", demoCollection.Take(10..^10).ToPrintString());
+            WriteLine();
+
+            WriteLine("Support for Default in FirstOrDefault/LastOrDefault/SingleOrDefault");
+            WriteLine("Enumerable.FirstOrDefault(defaultValue) {0}:", demoCollection.FirstOrDefault(x => x.id > 50, new User(-1,"DefaultUser")));
+            WriteLine("Enumerable.LastOrDefault(defaultValue) : {0}", demoCollection.LastOrDefault(x => x.id > 50, new User(-1, "DefaultUser")));
+            WriteLine("Enumerable.SingleOrDefault(defaultValue) : {0}", demoCollection.SingleOrDefault(x => x.id > 50, new User(-1, "DefaultUser")));
+
+            WriteLine("Support for Key Selector in DistinctBy");
+            WriteLine("Enumerable.DistinctBy(User.UserName) {0}", demoCollection.DistinctBy(x => x.Name).ToPrintString());
+
+            var secondCollection = GetData(10);
+            //WriteLine("Enumerable.ExceptBy(User.UserName) {0}", demoCollection.ExceptBy(secondCollection,user=>user.Name).ToPrintString());
+            WriteLine("Enumerable.ExceptBy(User.UserName) {0}", demoCollection.UnionBy(secondCollection,user=>user.Name).ToPrintString());
+
+
+            ReadLine();
        }
+
+        private static IEnumerable<User> GetData(int maxCount)
+        {
+            var random = new Random();
+            for (int i = 0; i < maxCount; i++)
+                yield return new User(i, $"User{random.Next(1,5)}");
+        }
+
 
         
         
