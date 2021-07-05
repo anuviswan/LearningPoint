040385f77ee6f453a69a8bd3510832d98ab88884
Anu Viswan <anu.viswan@gmail.com>
2021-07-05 05:56:20 +0530

    cleanup

--- Design Patterns/EventAggregator/IHandleAsync.cs
+++ Design Patterns/EventAggregator/IHandleAsync.cs
@@ -1,8 +1,4 @@
 ﻿using EventAggregator.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EventAggregator
