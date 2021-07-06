d071e14f5d9ee8f71afcfb060b3a864a879c6661
Anu Viswan <anu.viswan@gmail.com>
2021-07-06 05:45:58 +0530

    .Net core app

--- AspNetCore/MyFirstApp/Startup.cs
+++ AspNetCore/MyFirstApp/Startup.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyFirstApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstApp
+namespace MyFirstApp
 {
     public class Startup
     {
@@ -22,7 +24,8 @@ namespace FirstApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddSingleton<ILog, ConsoleLogger>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,18 +37,27 @@ namespace FirstApp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
-
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            app.UseWelcomePage();
+
+            app.Use(async (context,next) =>
+            {
+                await context.Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes("Hello"));
+                await next();
+            });
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
