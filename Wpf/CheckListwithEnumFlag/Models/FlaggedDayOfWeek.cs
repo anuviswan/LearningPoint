5d2d5c5f719be1e6ef241c000f3fbad86fda08d2
Anu Viswan <anu.viswan@gmail.com>
2021-07-10 18:15:11 +0530

    CheckListEnumDemo

---
+++ Wpf/CheckListwithEnumFlag/Models/FlaggedDayOfWeek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CheckListwithEnumFlag.Models
+{
+    [Flags]
+    public enum FlaggedDayOfWeek
+    {
+        None = 0x0,
+        Sunday = 0x1,
+        Monday = 0x2,
+        Tuesday = 0x4,
+        Wednesday = 0x8,
+        Thursday = 0x16,
+        Friday = 0x32,
+        Saturday= 0x61
+    }
+}
---
+++ Wpf/CheckListwithEnumFlag/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using CheckListwithEnumFlag.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckListwithEnumFlag.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        public FlaggedDayOfWeek SelectedWeekOfDay { get; set; }
+    }
+}
