2c2ed321e5e6b483513ca475902fb9f36800df31
Anu Viswan <anu.viswan@gmail.com>
2021-07-29 08:06:50 +0530

    Minimal web api example

---
+++ AspNetCore/MinimalWebAPi/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", (Func<string>)(() => "Hello World!"));
+app.MapGet("/sayhello", async (http) => {
+    await http.Response.WriteAsJsonAsync("Hello");
+});
+
+app.Run();
