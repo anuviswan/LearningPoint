96d2bf10321a71b39baaddfb44eb37ceb9530095
Anu Viswan <anu.viswan@gmail.com>
2021-07-30 05:01:45 +0530

    Implemented Jwt Authentication using Minimalist API

--- AspNetCore/MinimalWebAPi/Program.cs
+++ AspNetCore/MinimalWebAPi/Program.cs
@@ -1,13 +1,26 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.Linq;
+
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ITokenService>(new TokenService());
+builder.Services.AddSingleton<IUserRepositoryService>(new UserRepositoryService());
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new ()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 await using var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
@@ -15,9 +28,22 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", (Func<string>)(() => "This a demo for JWT Authentication using Minimalist Web API"));
-app.MapGet("/sayhello", async (http) => {
-    await http.Response.WriteAsJsonAsync("Hello");
+
+app.MapGet("/login", [AllowAnonymous] async (HttpContext http,ITokenService tokenService,IUserRepositoryService userRepositoryService) => {
+    var userModel = await http.Request.ReadFromJsonAsync<UserModel>();
+    var userDto = userRepositoryService.GetUser(userModel);
+    if (userDto == null)
+    {
+        http.Response.StatusCode = 401;
+        return;
+    }
+
+    var token = tokenService.BuildToken(builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"], userDto);
+    await http.Response.WriteAsJsonAsync(new { token = token });
+    return;
 });
+
+app.MapGet("/doaction", (Func<string>)([Authorize]() => "Action Succeeded"));
 
 await app.RunAsync();
 
@@ -36,7 +62,6 @@ public interface IUserRepositoryService
 {
     UserDto GetUser(UserModel userModel);
 }
-
 public class UserRepositoryService : IUserRepositoryService
 {
     private List<UserDto> _users => new ()
@@ -49,5 +74,29 @@ public class UserRepositoryService : IUserRepositoryService
     public UserDto GetUser(UserModel userModel)
     {
         return _users.FirstOrDefault(x => string.Equals(x.UserName,userModel.UserName) && string.Equals(x.Password, userModel.Password));
+    }
+}
+
+public interface ITokenService
+{
+    string BuildToken(string key, string issuer, UserDto user);
+}
+public class TokenService : ITokenService
+{
+    private TimeSpan ExpiryDuration = new TimeSpan(0, 30, 0);
+    public string BuildToken(string key, string issuer, UserDto user)
+    {
+        var claims = new[]
+        {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier,
+                Guid.NewGuid().ToString())
+             };
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new JwtSecurityToken(issuer, issuer, claims,
+            expires: DateTime.Now.Add(ExpiryDuration), signingCredentials: credentials);
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
     }
 }
---
+++ AspNetCore/MinimalWebAPi/Usings.cs
@@ -0,0 +1,14 @@
+﻿global using System;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Hosting;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Security.Claims;
+global using System.Text;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Authorization; 
