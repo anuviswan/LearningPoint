3865503c44a5b045a39d19348a46aef7d63fae6f
Anu Viswan <anu.viswan@gmail.com>
2021-08-06 20:17:06 +0530

    Embed notepad in wpf app

---
+++ Wpf/ContainerApplication/BootstrapperSc.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using ContainerApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContainerApplication
+{
+    public class BootstrapperSc : BootstrapperBase
+    {
+        #region Private Variable
+        private SimpleContainer _Container = new SimpleContainer();
+        #endregion
+
+        #region Constructor
+        public BootstrapperSc()
+        {
+            Initialize();
+        }
+        #endregion
+
+        #region Overrides
+        protected override void Configure()
+        {
+            _Container.Instance<IWindowManager>(new WindowManager());
+            _Container.Singleton<IEventAggregator, EventAggregator>();
+
+            // Hook up your ViewModel and Contract here
+             _Container.PerRequest<ShellViewModel, ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _Container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _Container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _Container.BuildUp(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+             DisplayRootViewFor<ShellViewModel>();
+        }
+        #endregion
+    }
+}
---
+++ Wpf/ContainerApplication/ViewModels/ShellViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace ContainerApplication.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        static extern bool MoveWindow(IntPtr Handle, int x, int y, int w, int h, bool repaint);
+
+        static readonly int GWL_STYLE = -16;
+        static readonly int WS_VISIBLE = 0x10000000;
+
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            var process = Process.Start("Notepad.exe");
+            process.WaitForInputIdle();
+            var win = view as Window;
+            var winHandle = new WindowInteropHelper(win).Handle;
+            SetParent(process.MainWindowHandle, winHandle);
+            SetWindowLong(process.MainWindowHandle, GWL_STYLE, WS_VISIBLE);
+            MoveWindow(process.MainWindowHandle, 0, 0, (int)win.ActualWidth, (int)win.ActualHeight, true);
+        }
+    }
+
+}
