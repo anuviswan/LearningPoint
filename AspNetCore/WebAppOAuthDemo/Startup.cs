5757feb78489baf176680679773e7dd3f2e2a429
Anu Viswan <anu.viswan@gmail.com>
2021-08-19 08:58:56 +0530

    OAuth Authentication implementation

--- AspNetCore/WebAppOAuthDemo/Startup.cs
+++ AspNetCore/WebAppOAuthDemo/Startup.cs
@@ -34,6 +34,15 @@ namespace WebAppOAuthDemo
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+
+                    options.ClientId = googleAuthNSection["ClientId"];
+                    options.ClientSecret = googleAuthNSection["ClientSecret"];
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
