a2312d5fced7a4764358f73798e97ff31d308dfe
Anu Viswan <anu.viswan@gmail.com>
2021-09-28 08:26:40 +0530

    Working on PriopertyTypeDescriptor

---
+++ Wpf/CustomTypeConverterDemo/Common/CustomPropertyTypeDescripter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTypeConverterDemo.Common
+{
+    public class CustomPropertyTypeDescripter : PropertyDescriptor
+    {
+        public CustomPropertyTypeDescripter(string name, Attribute[] attrs) : base(name, attrs)
+        {
+        }
+
+        public override Type ComponentType => throw new NotImplementedException();
+
+        public override bool IsReadOnly => throw new NotImplementedException();
+
+        public override Type PropertyType => throw new NotImplementedException();
+
+        public override bool CanResetValue(object component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetValue(object component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResetValue(object component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetValue(object component, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ShouldSerializeValue(object component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
