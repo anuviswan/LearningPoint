292700527c78af2fbc389563b03e3fdab83048fd
Anu Viswan <anu.viswan@gmail.com>
2021-11-16 21:11:23 +0530

    Adding hello world samples for RabbitMq

---
+++ RabbitMq/HelloWorld/RabbitMq.Producer/RabbitMq.Producer/Program.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+
+Console.WriteLine("Starting Hello World Producer...");
+var factory = new ConnectionFactory
+{
+    HostName = "my-rabbit-host"
+};
+
+using(var connection = factory.CreateConnection())
+{
+    using(var channel = connection.CreateModel())
+    {
+
+    }
+}
