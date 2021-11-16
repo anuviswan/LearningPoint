adf31fd12b77f97036598d6103a8d10cc767999c
Anu Viswan <anu.viswan@gmail.com>
2021-11-16 21:24:22 +0530

    Setting up Producer and Consumer

--- RabbitMq/HelloWorld/RabbitMq.Consumer/Program.cs
+++ RabbitMq/HelloWorld/RabbitMq.Consumer/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Starting Hello World Consumer...");
+
+var factory = new ConnectionFactory
+{
+    HostName = "my-rabbit-host"
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "Asgard",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Recieved message from Earth : {message}");
+        };
+
+        channel.BasicConsume("Asgard",true,consumer);
+    }
+}
+
+Console.WriteLine("Press [enter] to exit");
+Console.ReadLine();
--- RabbitMq/HelloWorld/RabbitMq.Producer/RabbitMq.Producer/Program.cs
+++ RabbitMq/HelloWorld/RabbitMq.Producer/RabbitMq.Producer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using System.Text;
 
 Console.WriteLine("Starting Hello World Producer...");
 var factory = new ConnectionFactory
@@ -10,6 +11,23 @@ using(var connection = factory.CreateConnection())
 {
     using(var channel = connection.CreateModel())
     {
+        channel.QueueDeclare(queue: "Asgard",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
 
+        var message = "Hello World !!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: String.Empty,
+            routingKey: "hello",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine($"Message :'{message}' Send....");
     }
 }
+
+Console.WriteLine("Press [enter] to exit");
+Console.ReadLine();
