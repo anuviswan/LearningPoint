e78b269f9373b929bcf6a853b104819a23aa181a
Anu Viswan <anu.viswan@gmail.com>
2021-11-18 02:22:11 +0530

    Worker queue example Adding Producer

---
+++ RabbitMq/WorkerQueues/WorkerQueue.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Starting Worker Queue Producer....");
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue:"AsgardWorkerQueue", 
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+if (args.Any())
+{
+    var message = CreateMessage(Convert.ToInt32(args[0]));
+    var body = Encoding.UTF8.GetBytes(message);
+
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    channel.BasicPublish("", "AsgardWorkerQueue", properties, body);
+    Console.WriteLine($"Published message :{message} to Asgard.");
+}
+
+Console.WriteLine("Exiting Worker Queue Producer.");
+
+
+string CreateMessage(int complexity)
+{
+    return $"Message from Thor {new string('.', complexity)}";
+}
