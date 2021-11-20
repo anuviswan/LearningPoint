27c4e8e1f6f3a5a584dbf3601fd4c6b9a9537f9f
Anu Viswan <anu.viswan@gmail.com>
2021-11-20 21:21:56 +0530

    fanout example

---
+++ RabbitMq/PublishSubscriber/Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Starting Publisher....");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+if (args.Length == 0)
+{
+    Console.WriteLine("No message found");
+};
+
+var message = args.First();
+
+Console.WriteLine($"Processing message : {message}");
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "logs", routingKey: "", body: body);
+
+Console.WriteLine($"Sending message : {message}");
---
+++ RabbitMq/PublishSubscriber/Subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Starting subscriber....");
+
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName, exchange: "logs", routingKey:"");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Recieved Message:{message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+Console.ReadLine();
