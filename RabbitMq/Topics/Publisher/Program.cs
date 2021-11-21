d5dbd86e77c227aabd5baa062ef7cf91153cf230
Anu Viswan <anu.viswan@gmail.com>
2021-11-21 22:03:30 +0530

    Working on topic example

---
+++ RabbitMq/Topics/Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+if (args.Count() != 2) return;
+
+
+var routingKey = args[0];
+var message = args[1];
+Console.WriteLine($"Starting Topic Logger for {args.First()}");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("TopicLogs", ExchangeType.Topic);
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("TopicLogs",routingKey,null,body);
+
+Console.WriteLine($"Sending message `{message}` with RoutingKey `{routingKey}` ");
---
+++ RabbitMq/Topics/Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+if (!args.Any()) return;
+
+Console.WriteLine($"Starting logger for {string.Join(",",args)}");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("TopicLogs", ExchangeType.Topic);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+foreach(var key in args)
+{
+    channel.QueueBind(queueName, "TopicLogs", key);
+}
+
+Console.WriteLine("Waiting for message");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = args.RoutingKey;
+
+    Console.WriteLine($"Recieved Message : '{message}' with key '{routingKey}");
+};
+
+channel.BasicConsume(queueName,true,consumer);
+Console.ReadLine();
