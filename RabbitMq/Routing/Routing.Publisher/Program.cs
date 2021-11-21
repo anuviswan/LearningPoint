f254704f7d9b1af88e1d9b4bb99ed2337b5bae0a
Anu Viswan <anu.viswan@gmail.com>
2021-11-21 10:43:40 +0530

    Filtered Channels

---
+++ RabbitMq/Routing/Routing.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Starting Routing Publisher....");
+if (!args.Any()) return;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("filteredLogs", ExchangeType.Direct);
+
+var severity = args[0];
+var message = args[1];
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "filteredLogs",routingKey:severity,body:body);
+Console.WriteLine($"Sending Message `{message}` with severity `{severity}` to the exchange");
+
+
---
+++ RabbitMq/Routing/Routing.Subscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+if (!args.Any()) return;
+
+Console.WriteLine($"Starting subscriber for {string.Join(",",args)}");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("filteredLogs", ExchangeType.Direct);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+foreach(var severity in args)
+{
+    channel.QueueBind(queueName, "filteredLogs", severity);
+}
+
+Console.WriteLine("Waiting for message");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (s, a) =>
+{
+    var body = a.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Recived message {message}");
+};
+
+channel.BasicConsume(queue:queueName,autoAck:true, consumer: consumer);
+
+Console.ReadLine();
