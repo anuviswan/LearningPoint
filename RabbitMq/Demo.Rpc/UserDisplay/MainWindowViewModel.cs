40aa1165778f7e449399e4afd1cae5f7a96fa875
Anu Viswan <anu.viswan@gmail.com>
2021-11-28 20:49:18 +0530

    Working on RPC demo code

---
+++ RabbitMq/Demo.Rpc/UserDisplay/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace UserDisplay
+{
+    internal class MainWindowViewModel
+    {
+        public IEnumerable<string> UserNames { get; set; }
+
+        public int Count { get; set; } = 15;
+    }
+}
