c4617bcd974717706239a51cba759bb8dacb45f8
Anu Viswan <anu.viswan@gmail.com>
2021-12-02 06:18:56 +0530

    Benchmark Foreach with IList, List and IEnumerable

---
+++ Benchmarking/BenchmarkForEach/BenchmarkForEach/LoopTest.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkForEach
+{
+    public class LoopTest
+    {
+        [Benchmark]
+        [ArgumentsSource(nameof(IEnumerableCollection))]
+        public void ForEachIEnumerable(IEnumerable<int> collection)
+        {
+            foreach (var item in collection)
+            {
+                DoSomething(item);
+            }
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(IListCollection))]
+        public void ForEachIList(IList<int> collection)
+        {
+            foreach (var item in collection)
+            {
+                DoSomething(item);
+            }
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ListCollection))]
+        public void ForEachList(List<int> collection)
+        {
+            foreach (var item in collection)
+            {
+                DoSomething(item);
+            }
+        }
+
+        public void DoSomething(int num)
+        {
+
+        }
+
+
+        public IEnumerable<IEnumerable<int>> IEnumerableCollection()
+        {
+            yield return Enumerable.Range(1, 99999); 
+        }
+        public IEnumerable<IList<int>> IListCollection ()
+        {
+            yield return Enumerable.Range(1, 99999).ToList();
+        }
+        public IEnumerable<List<int>> ListCollection()
+        {
+            yield return Enumerable.Range(1, 99999).ToList();
+        }
+    }
+}
---
+++ Benchmarking/BenchmarkForEach/BenchmarkForEach/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkForEach;
+
+var summary = BenchmarkRunner.Run<LoopTest>();
