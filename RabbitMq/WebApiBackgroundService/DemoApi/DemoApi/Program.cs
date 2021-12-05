c7647012248cfe7cc790e1485133e08d57c375bb
Anu Viswan <anu.viswan@gmail.com>
2021-12-05 21:20:04 +0530

    Demo for processing queue from Background Service in Web API Core

---
+++ RabbitMq/WebApiBackgroundService/DemoApi/DemoApi/Program.cs
@@ -0,0 +1,27 @@
+using DemoApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<QueueConsumerService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RabbitMq/WebApiBackgroundService/DemoApi/DemoApi/Services/QueueConsumerService.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace DemoApi.Services;
+public class QueueConsumerService:BackgroundService
+{
+    private const string CommandQueueName = "UserCommandQueue";
+    private IConnection _connection;
+    private IModel _channel;
+    public QueueConsumerService()
+    {
+        InitializeQueue();
+    }
+
+    private void InitializeQueue()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _channel.QueueDeclare(queue: CommandQueueName, true,false,false,null);
+
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (s, a) =>
+        {
+            var replyQueueName = a.BasicProperties.ReplyTo;
+
+            var messageFromClient = Encoding.UTF8.GetString(a.Body.ToArray());
+
+            var message = $"Mock Reply from the Background Service : Reply for '{messageFromClient}'";
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange:"",routingKey:replyQueueName,null,body);
+        };
+
+        _channel.BasicConsume(CommandQueueName, true, consumer);
+        return Task.CompletedTask;
+    }
+}
