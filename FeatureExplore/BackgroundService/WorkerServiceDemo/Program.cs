db8aef36fcf657001f9ef11976e713d559316909
Anu Viswan <anu.viswan@gmail.com>
2021-12-06 20:09:24 +0530

    Adding demo code for worker service

---
+++ FeatureExplore/BackgroundService/WorkerServiceDemo/Program.cs
@@ -0,0 +1,10 @@
+using WorkerServiceDemo;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
