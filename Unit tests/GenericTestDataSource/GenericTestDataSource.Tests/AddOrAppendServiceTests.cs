88266d3230b47901e63c8ac185834f1dae4cdcb1
Anu Viswan <anu.viswan@gmail.com>
2022-01-12 11:02:22 +0530

    better solution

--- Unit tests/GenericTestDataSource/GenericTestDataSource.Tests/AddOrAppendServiceTests.cs
+++ Unit tests/GenericTestDataSource/GenericTestDataSource.Tests/AddOrAppendServiceTests.cs
@@ -4,45 +4,28 @@ using System.Collections.Generic;
 using Xunit;
 
 namespace GenericTestDataSource.Tests;
+
 public class AddOrAppendServiceTests
 {
     [Theory]
-    [ClassData(typeof(TestCaseData))]
-    public void AddOrAppendService_Execute_Test(IServiceTestRunner tester)
+    [ClassData(typeof(GenericTestCaseData))]
+    public void AddOrAppendService_Execute_Test1<T>(IAddOrAppendService<T> addOrAppendService,T first,T second, T expected)
     {
-       tester.ExecuteTests();
+        var result = addOrAppendService.Execute(first, second);
+        Assert.Equal(expected, result);
     }
 }
 
-public interface IServiceTestRunner
-{
-    void ExecuteTests();
-}
-
-public class ServiceTestRunner<T> : IServiceTestRunner 
-{
-    public IAddOrAppendService<T> SUT { get; set; }
-    public T First { get; set; }
-    public T Second { get; set; }
-    public T ExpectedResult { get; set; }
-    public void ExecuteTests()
-    {
-        var actualResult = SUT.Execute(First,Second);
-        Assert.Equal(ExpectedResult, actualResult);
-    }
-}
-public class TestCaseData : IEnumerable<object[]>
+public class GenericTestCaseData : IEnumerable<object[]>
 {
     public IEnumerator<object[]> GetEnumerator()
     {
         yield return new object[]
         {
-             new ServiceTestRunner<string> { SUT = new StringAddOrAppendService(), First = "First", Second = "Second", ExpectedResult = "FirstSecond" }
-        };
-
-        yield return new object[]
-        {
-             new ServiceTestRunner<int> { SUT = new IntAddOrAppendService(), First = 1, Second = 2, ExpectedResult = 3}
+            new StringAddOrAppendService(),
+            "First",
+            "Second",
+            "FirstSecond"
         };
     }
 
