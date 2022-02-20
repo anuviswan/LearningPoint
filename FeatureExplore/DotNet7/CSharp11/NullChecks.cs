9f7f9053142e3738765bd0c11817aea8a61f64a8
Anu Viswan <anu.viswan@gmail.com>
2022-02-20 15:01:38 +0530

    Csharp11

---
+++ FeatureExplore/DotNet7/CSharp11/NullChecks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp11
+{
+    public class NullChecks
+    {
+        private class CustomConfiguration : ManualConfig
+        {
+            public CustomConfiguration()
+            {
+                AddJob(Job.Default.WithRuntime(CoreRuntime.Core50));
+                AddJob(Job.Default.WithRuntime(CoreRuntime.Core60));
+            }
+        }
+        [Benchmark]
+        public void NullCheckUsingCSharp10(string name)
+        {
+            ArgumentNullException.ThrowIfNull(name);
+        }
+
+    }
+}
---
+++ FeatureExplore/DotNet7/CSharp11/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("Hello, World!");
+Foo(null);
+
+static void Foo(string name)
+{
+}
+
+
+public class Bar
+{
+    [Foo<long>]
+    public long Id { get; set; }
+
+    [Foo<string>]
+    public string Name { get; set; }
+
+}
+public class FooAttribute<T> : Attribute
+{
+
+}
