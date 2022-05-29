a402f5cb4e675a1cd3084e61f51dee3c4aa1cbd2
anu.viswan@gmail.com <source@2030>
2022-05-29 13:11:33 +0530

    Added support for Retry with Polly

--- Cloud Patterns/RetryPattern/RetryPatternWithPolly/Program.cs
+++ Cloud Patterns/RetryPattern/RetryPatternWithPolly/Program.cs
@@ -1,9 +1,22 @@
+using Polly;
+using Polly.Extensions.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("Demo")
+        .AddPolicyHandler(GetRetryPolicy()); ;
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+         .HandleTransientHttpError()
+         .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+         .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,
+                                                                     retryAttempt)));
+}
 
 var app = builder.Build();
 
@@ -34,6 +47,17 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+
+app.MapGet("/retrydemo", async (IHttpClientFactory clientFactory) =>
+{
+    var url = $"https://jsonplaceholder.typicode.com/todos/1";
+
+    var client = clientFactory.CreateClient("Demo");
+    var response = await client.GetAsync(url);
+    var result = await response.Content.ReadAsStringAsync();
+    return result;
+}).WithName("RetryDemo");
 
 app.Run();
 
