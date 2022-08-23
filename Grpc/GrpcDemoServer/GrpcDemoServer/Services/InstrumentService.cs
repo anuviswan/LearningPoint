071ddcbeb03f05cb7acdefacfb04fd81aee01118
Anu Viswan <anu.viswan@gmail.com>
2022-08-23 21:54:07 +0530

    Added sample for streaming server response

--- Grpc/GrpcDemoServer/GrpcDemoServer/Services/InstrumentService.cs
+++ Grpc/GrpcDemoServer/GrpcDemoServer/Services/InstrumentService.cs
@@ -11,4 +11,17 @@ public class InstrumentService: Instrument.InstrumentBase
             Status = $"Connected : {TimeOnly.FromDateTime(DateTime.Now).ToLongTimeString()}"
         }) ;
     }
+
+    public override async Task Subscribe(RawDataRequest request, IServerStreamWriter<RawDataResponse> responseStream, ServerCallContext context)
+    {
+        for(int i = 0; i < request.MaxItems; i++)
+        {
+            await Task.Delay(1000);
+            await responseStream.WriteAsync(new RawDataResponse
+            {
+                Id = i,
+                Description = $"Description {i}"
+            });
+        }
+    }
 }
