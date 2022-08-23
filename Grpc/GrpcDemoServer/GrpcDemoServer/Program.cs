448f7f105f2d14e42cefa1b2720b69a9a90ee071
Anu Viswan <anu.viswan@gmail.com>
2022-08-23 21:46:15 +0530

    added support for server reflection

--- Grpc/GrpcDemoServer/GrpcDemoServer/Program.cs
+++ Grpc/GrpcDemoServer/GrpcDemoServer/Program.cs
@@ -1,4 +1,5 @@
 using GrpcDemoServer.Services;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<InstrumentService>();
+app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
