68955ff0fbbb1d8187beddc478dd4265a51da10d
Anu Viswan <anu.viswan@beo.in>
2022-09-13 09:11:41 +0530

    added demo code for required

---
+++ FeatureExplore/CSharp11/RequiredMembers/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Demo code for 'required' modifier");
+
+var user = new User() { Name = "John Doe" };
+
+
+
+public class User
+{
+    public required string  Name{ get; init; }
+}
