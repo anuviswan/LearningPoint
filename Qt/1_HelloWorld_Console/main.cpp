7f120e30d17736f711e1d20267bdddbca9197011
Anu Viswan <anu.viswan@gmail.com>
2022-10-05 21:01:18 +0530

    demo

--- Qt/1_HelloWorld_Console/main.cpp
+++ Qt/1_HelloWorld_Console/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    qInfo() << ("Hello World Demo");
+
     char* name = "John Doe <Default Name>";
     if(argc == 1)
         qInfo() << ("Hello World %s",name);
