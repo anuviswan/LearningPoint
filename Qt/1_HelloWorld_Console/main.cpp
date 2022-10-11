d634ddab217ea8a5b7c94288eb0586e855087698
Anu Viswan <anu.viswan@gmail.com>
2022-10-11 21:34:44 +0530

    changes

--- Qt/1_HelloWorld_Console/main.cpp
+++ Qt/1_HelloWorld_Console/main.cpp
@@ -1,10 +1,10 @@
 #include <QCoreApplication>
 #include <QtDebug>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     qInfo("Hello World Demo");
 
     QString name = argc == 2 ? argv[1] : "John Doe <Default Name>";
