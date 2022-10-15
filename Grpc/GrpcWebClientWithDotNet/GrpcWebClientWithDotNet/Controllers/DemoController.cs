fb0af6f86cc14b9e5b26632588090a57dd4f399a
Anu Viswan <anu.viswan@gmail.com>
2022-10-15 20:51:35 +0530

    Added support for Grpc Web for sample demo

---
+++ Grpc/GrpcWebClientWithDotNet/GrpcWebClientWithDotNet/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using GrpcDemoServer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcWebClientWithDotNet.Controllers
+{
+    [Route("/[controller]")]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("SayHello")]
+        public async Task<IActionResult> SayHello()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7280", new GrpcChannelOptions
+            {
+                HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+            });
+
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = ".NET" });
+            return Ok(response.Message);
+        }
+    }
+}
