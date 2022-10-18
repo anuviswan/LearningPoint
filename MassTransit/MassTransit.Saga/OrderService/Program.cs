caca4af7ef0cec0e8ce6a0393d3bde0d4d77fdb7
Anu Viswan <anu.viswan@gmail.com>
2022-10-18 20:31:05 +0530

    Working on demo

--- MassTransit/MassTransit.Saga/OrderService/Program.cs
+++ MassTransit/MassTransit.Saga/OrderService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.OpenApi;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,29 +19,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/createorder", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    Debug.WriteLine("Creating Order");
+}).WithName("CreateOrder")
+  .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
