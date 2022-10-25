a6adf507a30e8392ff3d4f190b895f7218b4125b
Anu Viswan <anu.viswan@gmail.com>
2022-10-25 21:12:39 +0530

    Added Swagger Title, Description

--- MassTransit/MassTransit.Mediator/MassTransit.Mediator/Program.cs
+++ MassTransit/MassTransit.Mediator/MassTransit.Mediator/Program.cs
@@ -14,7 +14,15 @@ builder.Services.AddMediator(cfg =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(cfg=>
+{
+    cfg.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Mediator With MassTransit",
+        Description = "Demonstration to build a Mediator using MassTransit",
+        Version = "v1.0",
+    });
+});
 
 var app = builder.Build();
 
