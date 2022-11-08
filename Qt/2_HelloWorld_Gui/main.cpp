9bf8ea932d575449f65d705531ac0174513da81b
Anu Viswan <anu.viswan@gmail.com>
2022-11-08 20:40:28 +0530

    Updated Qt Widget Hello World Demo

--- Qt/2_HelloWorld_Gui/main.cpp
+++ Qt/2_HelloWorld_Gui/main.cpp
@@ -8,8 +8,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow mainWindow;
     mainWindow.show();
-
-    QPushButton btn("Click Me !!", &mainWindow);
-
     return a.exec();
 }
