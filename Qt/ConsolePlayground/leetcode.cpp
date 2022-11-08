3ce387e33d182c138e29874dfc6ac6d246f62c41
Anu Viswan <anu.viswan@gmail.com>
2022-11-08 08:20:37 +0530

    Update Leetcode demo

--- Qt/ConsolePlayground/leetcode.cpp
+++ Qt/ConsolePlayground/leetcode.cpp
@@ -8,13 +8,13 @@ LeetCode::LeetCode()
 
 vector<int> LeetCode::twoSum(vector<int> &nums, int target)
 {
-    qInfo() << "Recieved input " << nums;
-    qInfo() << "Recieved Target " << target;
     map<int,int> mapInt;
 
-    for(int i=0;i< sizeof(nums);i++){
+    for(int i=0;i< end(nums) - begin(nums) ;i++)
+    {
         int current = nums[i];
         int expected = target - current;
+
         std::map<int,int>::iterator iterate = mapInt.find(expected);
         if(iterate != mapInt.end())
         {
