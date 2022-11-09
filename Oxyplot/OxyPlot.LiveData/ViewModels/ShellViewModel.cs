957971aa2c52aa75ef849af4590127fe19d26483
Anu Viswan <anu.viswan@gmail.com>
2022-11-09 06:26:09 +0530

    Working on demo for live data in oxyplot

--- Oxyplot/OxyPlot.LiveData/ViewModels/ShellViewModel.cs
+++ Oxyplot/OxyPlot.LiveData/ViewModels/ShellViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Caliburn.Micro;
 using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using System;
+using System.Linq;
 using System.Windows.Threading;
 
 namespace OxyPlot.LiveData.ViewModels;
@@ -9,13 +12,57 @@ public class ShellViewModel:Screen
 {
     private DispatcherTimer? _timer;
     private Random _randomGenerator;
-    public BindableCollection<SensorData> SensorData { get; set; } = new BindableCollection<SensorData>();
+    public BindableCollection<SensorData> SensorData { get; set; }
     public PlotModel SensorPlotModel { get; set; }
 
     public ShellViewModel()
     {
-        SensorPlotModel = new PlotModel();
+        InitializePlotModel();
         _randomGenerator = new Random();
+        SensorData = new BindableCollection<SensorData>();
+        SensorData.CollectionChanged += SensorData_CollectionChanged;
+    }
+
+    public void InitializePlotModel()
+    {
+        SensorPlotModel = new() 
+        { 
+            Title = "Demo Live Tracking",
+        };
+
+        SensorPlotModel.Axes.Add(new DateTimeAxis
+        {
+            Title = "TimeStamp",
+            Position = AxisPosition.Bottom,
+        });
+
+        SensorPlotModel.Axes.Add(new LinearAxis
+        {
+            Title = "Data Value",
+            Position = AxisPosition.Left
+        });
+
+        SensorPlotModel.Series.Add(new LineSeries
+        {
+            DataFieldX = "X",
+            DataFieldY = "Y"
+        });
+    }
+
+    private void SensorData_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems is null) return;
+
+        var series = SensorPlotModel.Series.OfType<LineSeries>().First();
+        foreach (var newItem in e.NewItems)
+        {
+            if(newItem is SensorData sensorData)
+            {
+                series.Points.Add(new DataPoint(sensorData.Data, DateTimeAxis.ToDouble(sensorData.TimeStamp)));
+            }
+        }
+
+        NotifyOfPropertyChange(nameof(SensorPlotModel));
     }
 
     public bool CanStartAcquisition => _timer?.IsEnabled != true;
