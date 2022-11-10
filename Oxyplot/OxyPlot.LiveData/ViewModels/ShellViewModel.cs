2048f62fae2cba5558c7f189deb41f8276f38b02
Anu Viswan <anu.viswan@gmail.com>
2022-11-10 05:58:54 +0530

    Streaming view in oxyplot demo

--- Oxyplot/OxyPlot.LiveData/ViewModels/ShellViewModel.cs
+++ Oxyplot/OxyPlot.LiveData/ViewModels/ShellViewModel.cs
@@ -2,14 +2,19 @@
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
+using OxyPlot.Wpf;
 using System;
 using System.Linq;
 using System.Windows.Threading;
+using DateTimeAxis = OxyPlot.Axes.DateTimeAxis;
+using LinearAxis = OxyPlot.Axes.LinearAxis;
+using LineSeries = OxyPlot.Series.LineSeries;
 
 namespace OxyPlot.LiveData.ViewModels;
 
 public class ShellViewModel:Screen
 {
+    private const int MaxSecondsToShow = 20;
     private DispatcherTimer? _timer;
     private Random _randomGenerator;
     public BindableCollection<SensorData> SensorData { get; set; }
@@ -34,18 +39,30 @@ public class ShellViewModel:Screen
         {
             Title = "TimeStamp",
             Position = AxisPosition.Bottom,
+            StringFormat = "HH:mm:ss",
+            IntervalLength = 60,
+            Minimum = DateTimeAxis.ToDouble(DateTime.Now),
+            Maximum = DateTimeAxis.ToDouble(DateTime.Now.AddSeconds(MaxSecondsToShow)),
+            IsPanEnabled = true,
+            IsZoomEnabled = true,
+            IntervalType = DateTimeIntervalType.Seconds,
+            MajorGridlineStyle = LineStyle.Solid,
+            MinorGridlineStyle = LineStyle.Solid,
         });
 
         SensorPlotModel.Axes.Add(new LinearAxis
         {
             Title = "Data Value",
-            Position = AxisPosition.Left
+            Position = AxisPosition.Left,
+            IsPanEnabled = true,
+            IsZoomEnabled = true,
+            Minimum = 0,
+            Maximum = 1
         });
 
-        SensorPlotModel.Series.Add(new LineSeries
+        SensorPlotModel.Series.Add(new LineSeries()
         {
-            DataFieldX = "X",
-            DataFieldY = "Y"
+            MarkerType = MarkerType.Circle,
         });
     }
 
@@ -54,13 +71,33 @@ public class ShellViewModel:Screen
         if (e.NewItems is null) return;
 
         var series = SensorPlotModel.Series.OfType<LineSeries>().First();
+
+        var dateTimeAxis = SensorPlotModel.Axes.OfType<DateTimeAxis>().First();
+
+        if (!series.Points.Any())
+        {
+            dateTimeAxis.Minimum = DateTimeAxis.ToDouble(DateTime.Now);
+            dateTimeAxis.Maximum = DateTimeAxis.ToDouble(DateTime.Now.AddSeconds(MaxSecondsToShow));
+        }
+
         foreach (var newItem in e.NewItems)
         {
             if(newItem is SensorData sensorData)
             {
-                series.Points.Add(new DataPoint(sensorData.Data, DateTimeAxis.ToDouble(sensorData.TimeStamp)));
+                series.Points.Add(new DataPoint(DateTimeAxis.ToDouble(sensorData.TimeStamp), sensorData.Data));
             }
         }
+
+        
+
+        if (DateTimeAxis.ToDouble(DateTime.Now) > dateTimeAxis.Maximum)
+        {
+            dateTimeAxis.Minimum = DateTimeAxis.ToDouble(DateTime.Now.AddSeconds(-1 * MaxSecondsToShow));
+            dateTimeAxis.Maximum = DateTimeAxis.ToDouble(DateTime.Now);
+            dateTimeAxis.Reset();
+        }
+
+        SensorPlotModel.InvalidatePlot(true);
 
         NotifyOfPropertyChange(nameof(SensorPlotModel));
     }
