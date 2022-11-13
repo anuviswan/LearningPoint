55b41c2f84ceff88509932d327e958f46dd63e5a
Anu Viswan <anu.viswan@gmail.com>
2022-11-13 20:31:55 +0530

    Adding Saga demo

---
+++ Microservices/SagaDemo/OrderService/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.OpenApi;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/createorder", () =>
+{
+    Debug.WriteLine("Creating Order");
+}).WithName("CreateOrder")
+  .WithOpenApi();
+
+app.Run();
