2817beca5e4837fa0b2083219a8fa9d981a9c5e0
Anu Viswan <anu.viswan@gmail.com>
2022-12-17 12:35:41 +0530

    Fibonacci with IGen

--- FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/IFibGen.cs
+++ FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/IFibGen.cs
@@ -1,18 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StaticVirtualInterfaceMembers
 {
-    public interface IFibGen<T> : where T is IFibGen<T>
+    public interface IGen<T>  where T : IGen<T>
     {
-        static abstract operator ++(T other);
+        static abstract T operator ++(T val);
     }
 
-    public class Fibonacii : IFibGen<Fibonacii>
+
+   
+
+    public struct ValGen : IGen<ValGen>
     {
-        public static operator ++(Fibonacii other) { }
+        public int Value { get; set; }
+        public static ValGen operator ++(ValGen val)
+        {
+            return val with { Value = val.Value + 1 };
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
     }
+
+
+    public struct FibGen : IGen<FibGen>
+    {
+        public int Previous { get; set; }
+
+        public int Current { get; set; }
+
+
+        public override string ToString()
+        {
+            return Current.ToString();
+        }
+
+        public static FibGen operator ++(FibGen val)
+        {
+            return val with 
+            { 
+                Previous = val.Current,
+                Current = val.Previous + val.Current 
+            };
+        }
+    }
+
+    //public class Fibonacii : IFibGen<Fibonacii>
+    //{
+    //    public int Value { get; set; }
+    //    public static Fibonacii operator ++(Fibonacii first, Fibonacii second) 
+    //    { 
+    //        return default; 
+    //    }
+    //}
 }
--- FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/Program.cs
+++ FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/Program.cs
@@ -5,5 +5,16 @@ Console.WriteLine($"{nameof(Parrot)}: Can Fly - {Parrot.CanFly()}");
 Console.WriteLine($"{nameof(Kiwi)}: Can Fly - {Kiwi.CanFly()}");
 
 
+//var valGen = new ValGen { Value = 2 };
+//for (var i = 0; i < 10; i++)
+//    Console.WriteLine(valGen++);
+
+
+var fibGenOne = new FibGen { Previous = 0, Current = 1 };
+
+for(var i = 0; i < 10;i++)
+    Console.WriteLine(fibGenOne ++);
+
+
 
 
