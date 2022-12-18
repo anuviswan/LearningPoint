cf62d7754649346cf4f673921bd3fe4539443c7f
Anu Viswan <anu.viswan@gmail.com>
2022-12-18 12:05:12 +0530

    cleanup

--- FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/IFibGen.cs
+++ FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/IFibGen.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StaticVirtualInterfaceMembers
+﻿namespace StaticVirtualInterfaceMembers
 {
     public interface ISequenceGenerator<T>  where T : ISequenceGenerator<T>
     {
