d94d2d2285ffa538d36b71ed781fed006b0b057e
Anu Viswan <anu.viswan@gmail.com>
2022-12-19 20:31:42 +0530

    Updating example

--- FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/SimpleExample.cs
+++ FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/SimpleExample.cs
@@ -1,21 +1,9 @@
 ﻿namespace StaticVirtualInterfaceMembers
 {
-    public interface IBar
+    public interface IMeasureable<T>
     {
-        string Name { get; set; }
 
-        public string GetName() => Name ?? $"Cannot find {nameof(Name)}";
     }
 
-    public class Bar : IBar
-    {
-        public string Name { get; set; }
-    }
-
-    public class FooBar : IBar
-    {
-        public string Name { get; set; }
-
-        public string GetName() => "Bar";
-    }
+  
 }
