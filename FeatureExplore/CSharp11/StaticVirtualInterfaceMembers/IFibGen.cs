5ace501cb77e4127204d9d67dab71520f88ddf21
Anu Viswan <anu.viswan@beo.in>
2022-12-21 09:07:44 +0530

    Code cleanup

--- FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/IFibGen.cs
+++ FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/IFibGen.cs
@@ -2,7 +2,11 @@
 {
     public interface ISequenceGenerator<T>  where T : ISequenceGenerator<T>
     {
-       static abstract T operator ++(T val);
+        static abstract T Zero { get; }
+
+        static abstract T One { get; }
+       
+        static abstract T operator ++(T val);
     }
 
 
@@ -10,7 +14,11 @@
 
     public struct ValGen : ISequenceGenerator<ValGen>
     {
+
         public int Value { get; set; }
+
+        public static ValGen Zero => new ValGen { Value = 0 };
+        public static ValGen One => new ValGen { Value = 1 };
         public static ValGen operator ++(ValGen val)
         {
             return val with { Value = val.Value + 1 };
@@ -29,10 +37,14 @@
         public int Current { get; init; }
         public override string ToString() => Current.ToString();
 
+        public static FibonacciGenerator Zero => new FibonacciGenerator { Current = 0, Previous = 0 };
+
+        public static FibonacciGenerator One => new FibonacciGenerator { Current = 1, Previous = 0 };
+
         public static FibonacciGenerator operator ++(FibonacciGenerator val)
         {
-            if (val is { Previous: 0, Current: 0})
-                return val with { Current = 1 };
+            if (val.Equals(FibonacciGenerator.Zero))
+                return FibonacciGenerator.One;
             else
                 return val with 
                 { 
--- FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/Program.cs
+++ FeatureExplore/CSharp11/StaticVirtualInterfaceMembers/Program.cs
@@ -5,21 +5,16 @@ Console.WriteLine($"{nameof(Parrot)}: Can Fly - {Parrot.CanFly()}");
 Console.WriteLine($"{nameof(Kiwi)}: Can Fly - {Kiwi.CanFly()}");
 
 
-IBar bar = new Bar();
-Console.WriteLine($"Bar.GetName() == {bar.GetName()}");
-
-IBar fooBar = new FooBar();
-Console.WriteLine($"FooBar.GetName() == {fooBar.GetName()}");
 
 //var valGen = new ValGen { Value = 2 };
 //for (var i = 0; i < 10; i++)
 //    Console.WriteLine(valGen++);
 
 
-var fibGenOne = new FibonacciGenerator();
+var fibGenerator = FibonacciGenerator.Zero;
 
 for(var i = 0; i < 10;i++)
-    Console.Write($"{fibGenOne++}, ");
+    Console.Write($"{fibGenerator++}, ");
 
 
 
