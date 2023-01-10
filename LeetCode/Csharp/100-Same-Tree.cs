661a1270101d1e31d44195a443698fa1a072a146
Anu Viswan <anu.viswan@beo.in>
2023-01-10 09:03:25 +0530

    Added Question of day

---
+++ LeetCode/Csharp/100-Same-Tree.cs
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q == null) return true;
+        if(p == null || q == null ) return false;
+        
+        return (p.val == q.val && IsSameTree(p.left,q.left) && IsSameTree(p.right , q.right));
+    }
+}
