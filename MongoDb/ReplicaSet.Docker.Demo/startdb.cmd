fb3b44066a5b3a42261f7fb964f275fd1aca2514
Anu Viswan <anu.viswan@gmail.com>
2023-02-12 21:41:28 +0530

    Updated replica set docker compose and batch file

--- MongoDb/ReplicaSet.Docker.Demo/startdb.cmd
+++ MongoDb/ReplicaSet.Docker.Demo/startdb.cmd
@@ -4,4 +4,4 @@ docker-compose up -d
 
 timeout  /t 10 /nobreak
 
-docker exec -it mongo.primary mongosh --eval "rs.initiate({_id:'dbrs', members: [{_id:0, host: 'mongo.primary'},{_id:1, host: 'mongo.replica.one'},{_id:2, host: 'mongo.replica.two'}]})"
+docker exec -it mongo.one.db mongosh --eval "rs.initiate({_id:'dbrs', members: [{_id:0, host: 'mongo.one.db'},{_id:1, host: 'mongo.two.db'},{_id:2, host: 'mongo.three.db'}]})"
