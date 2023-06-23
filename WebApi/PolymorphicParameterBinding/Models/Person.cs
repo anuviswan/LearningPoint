2449193edee20198623218cf93bd9b408fac1ab7
Anu Viswan <anu.viswan@gmail.com>
2023-06-23 06:21:49 +0530

    Changed dto

--- WebApi/PolymorphicParameterBinding/Models/Person.cs
+++ WebApi/PolymorphicParameterBinding/Models/Person.cs
@@ -2,5 +2,19 @@
 {
     public class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; } 
+    }
+
+    public class Student
+    {
+        public string SchoolName { get; set; }
+
     }
 }
