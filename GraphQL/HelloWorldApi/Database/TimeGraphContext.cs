5b8dc3b3b3c84794efd35013b7c38e8cc9ae0490
Anu Viswan <anu.viswan@gmail.com>
2023-07-23 16:39:37 +0530

    updated web api

--- GraphQL/HelloWorldApi/Database/TimeGraphContext.cs
+++ GraphQL/HelloWorldApi/Database/TimeGraphContext.cs
@@ -1,53 +1,52 @@
 ﻿using HelloWorldApi.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace HelloWorldApi.Database
+namespace HelloWorldApi.Database;
+
+public class TimeGraphContext : DbContext
 {
-    public class TimeGraphContext : DbContext
+    public TimeGraphContext(DbContextOptions<TimeGraphContext> options) : base(options)
     {
-        public TimeGraphContext(DbContextOptions<TimeGraphContext> options) : base(options)
-        {
-            Database.EnsureCreated();
-        }
+        Database.EnsureCreated();
+    }
 
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<TimeLog> TimeLogs { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TimeLog> TimeLogs { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Project>().HasData(new Project
         {
-            modelBuilder.Entity<Project>().HasData(new Project
-            {
-                CreatedBy = "Giorgi",
-                Id = 1,
-                Name = "Migrate to TLS 1.2"
-            }, new Project
-            {
-                CreatedBy = "Giorgi",
-                Id = 2,
-                Name = "Move Blog to Hugo"
-            });
+            CreatedBy = "Giorgi",
+            Id = 1,
+            Name = "Migrate to TLS 1.2"
+        }, new Project
+        {
+            CreatedBy = "Giorgi",
+            Id = 2,
+            Name = "Move Blog to Hugo"
+        });
 
-            modelBuilder.Entity<TimeLog>().HasData(new TimeLog
-            {
-                Id = 1,
-                From = new DateTime(2020, 7, 24, 12, 0, 0),
-                To = new DateTime(2020, 7, 24, 14, 0, 0),
-                ProjectId = 1,
-                User = "Giorgi"
-            }, new TimeLog
-            {
-                Id = 2,
-                From = new DateTime(2020, 7, 24, 16, 0, 0),
-                To = new DateTime(2020, 7, 24, 18, 0, 0),
-                ProjectId = 1,
-                User = "Giorgi"
-            }, new TimeLog
-            {
-                Id = 3,
-                From = new DateTime(2020, 7, 24, 20, 0, 0),
-                To = new DateTime(2020, 7, 24, 22, 0, 0),
-                ProjectId = 2,
-                User = "Giorgi"
-            });
-        }
+        modelBuilder.Entity<TimeLog>().HasData(new TimeLog
+        {
+            Id = 1,
+            From = new DateTime(2020, 7, 24, 12, 0, 0),
+            To = new DateTime(2020, 7, 24, 14, 0, 0),
+            ProjectId = 1,
+            User = "Giorgi"
+        }, new TimeLog
+        {
+            Id = 2,
+            From = new DateTime(2020, 7, 24, 16, 0, 0),
+            To = new DateTime(2020, 7, 24, 18, 0, 0),
+            ProjectId = 1,
+            User = "Giorgi"
+        }, new TimeLog
+        {
+            Id = 3,
+            From = new DateTime(2020, 7, 24, 20, 0, 0),
+            To = new DateTime(2020, 7, 24, 22, 0, 0),
+            ProjectId = 2,
+            User = "Giorgi"
+        });
     }
 }
--- GraphQL/HelloWorldApi/Models/Project.cs
+++ GraphQL/HelloWorldApi/Models/Project.cs
@@ -1,9 +1,8 @@
-﻿namespace HelloWorldApi.Models
+﻿namespace HelloWorldApi.Models;
+
+public class Project
 {
-    public class Project
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string CreatedBy { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
 }
