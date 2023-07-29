dfd3ae1d8f0868094ca9ac89eb8d55df9b7f2801
Anu Viswan <anu.viswan@gmail.com>
2023-07-29 07:38:43 +0530

    Adding example for Day 3

---
+++ GraphQL/HotChocolate/Day 003/GraphQl/Queries/TimeLogQueryResolver.cs
@@ -0,0 +1,14 @@
+﻿using GraphQLDemo.Database;
+using GraphQLDemo.Models;
+
+namespace GraphQLDemo.GraphQl.Queries;
+
+[ExtendObjectType(typeof(Query))]
+public class TimeLogQueryResolver
+{
+    [GraphQLDescription("Fetch all timelog")]
+    public IEnumerable<TimeLog> FetchAllTimeLog([Service(ServiceKind.Synchronized)] DemoGraphContext dbContext)
+    {
+        return dbContext.TimeLogs;
+    }
+}
---
+++ GraphQL/HotChocolate/Day 003/Program.cs
@@ -0,0 +1,35 @@
+using GraphQLDemo.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DemoGraphContext>(context =>
+{
+    context.UseInMemoryDatabase("TimeGraphServer");
+});
+
+builder.Services.AddGraphQLServer()
+                .AddQueryType<QueryType>();
+
+var app = builder.Build();
+app.MapGraphQL();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
