2f7da6965fd8cd1a9b55a6f3b6faeb25c4b848a0
Anu Viswan <anu.viswan@gmail.com>
2023-07-30 20:07:20 +0530

    working on mutations - missing updates

--- GraphQL/HotChocolate/Day 003/GraphQl/Mutations/ProjectMutation.cs
+++ GraphQL/HotChocolate/Day 003/GraphQl/Mutations/ProjectMutation.cs
@@ -4,20 +4,5 @@ using GraphQLDemo.Models;
 [ExtendObjectType(typeof(Mutation))]
 public class ProjectMutation
 {
-    [UseMutationConvention]
-    [GraphQLDescription("Add Project")]
-    public async Task<Project> AddProject([Service(ServiceKind.Synchronized)] DemoGraphContext dbContext,
-        [ID] Guid id, string name, string createdBy )
-    {
-        var project = new Project
-        {
-            //Id = id,
-            Name = name,
-            CreatedBy = createdBy
-        };
-        var result  = await dbContext.AddAsync(project); 
-        
-        await dbContext.SaveChangesAsync();
-        return result.Entity;
-    } 
+    
 }
--- GraphQL/HotChocolate/Day 003/GraphQl/Queries/Query.cs
+++ GraphQL/HotChocolate/Day 003/GraphQl/Queries/Query.cs
@@ -1,5 +1,4 @@
-﻿namespace GraphQLDemo.GraphQl.Queries;
-
+﻿
 public class Query
 {
 }
