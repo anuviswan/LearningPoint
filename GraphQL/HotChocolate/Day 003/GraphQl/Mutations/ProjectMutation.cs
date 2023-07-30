27277c684ae2566f31b50e5936ca103ed6e88c1d
Anu Viswan <anu.viswan@gmail.com>
2023-07-30 19:06:09 +0530

    boiler plate mutations

--- GraphQL/HotChocolate/Day 003/GraphQl/Mutations/ProjectMutation.cs
+++ GraphQL/HotChocolate/Day 003/GraphQl/Mutations/ProjectMutation.cs
@@ -1,7 +1,12 @@
-﻿using GraphQLDemo.Models;
+﻿using GraphQLDemo.Database;
+using GraphQLDemo.Models;
 
 [ExtendObjectType(typeof(Mutation))]
 public class ProjectMutation
 {
-    public async Task<Project> AddProject()
+    public async Task<Project> AddProject([Service(ServiceKind.Synchronized)] DemoGraphContext dbContext,Project project)
+    {
+        await dbContext.AddAsync(project);
+        return project;
+    } 
 }
