91590645857ec5565bf36881cbc06aadc55a7a81
Anu Viswan <anu.viswan@gmail.com>
2023-09-04 21:29:39 +0530

    adding TristateCheckBox.vue

---
+++ vuejs/V009 - TristateCheckBox/v009/src/components/TristateCheckBox.vue
@@ -0,0 +1,29 @@
+<template>
+        <input type="checkbox" 
+        v-bind:model="modelValue" 
+        v-bind:checked="isChecked" 
+        v-bind:indeterminate="isIndeterminate"
+        @input="$emit('update:modelValue', isChecked)"
+        />
+
+
+</template>
+<script setup>
+import { onMounted, ref, defineProps } from 'vue';
+
+const props = defineProps({
+    modelValue: { type: Boolean, default: undefined }
+})
+
+const isChecked = ref<Boolean | undefined>(false)
+const isIndeterminate = ref(false)
+
+onMounted(() => {
+  if (props.modelValue == undefined) isIndeterminate.value = true
+  else isChecked.value = props.modelValue
+})
+
+</script>
+<style scoped>
+
+</style>
