145fdb7349e6c945199fea60ddf2c9cfe86ac0bb
Anu Viswan <anu.viswan@gmail.com>
2023-10-15 20:07:20 +0530

    Adding apollo

--- GraphQL/Vue-Apollo/democlient1/src/main.js
+++ GraphQL/Vue-Apollo/democlient1/src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-createApp(App).mount('#app')
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+app.mount('#app')
