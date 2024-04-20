9e82c0a825f1337d2fd6a0586076a206686393d2
Anu Viswan <anu.viswan@gmail.com>
2024-04-20 21:43:04 +0530

    Added more examples of Primary constructor
    Added comments

--- FeatureExplore/CSharp12/CSharp12CoreFeatures/PrimaryConstructorDemo.cs
+++ FeatureExplore/CSharp12/CSharp12CoreFeatures/PrimaryConstructorDemo.cs
@@ -1,7 +1,32 @@
 ﻿namespace CSharp12CoreFeatures;
 
+
+public class FooWithoutPrimaryConstructor
+{
+    public string Name { get; init; }
+    public string Address { get; init; }
+
+
+    public FooWithoutPrimaryConstructor(string name,string address)
+    {
+        Name = name;
+        Address = address;
+    }
+}
+
+// Primary Constructor eliminates the need for explicit constructor,
+// with sole purpose of assigning values to private fields/properties
+public class FooWithPrimaryConstructor(string name, string address)
+{
+    public string Name { get; init; } = name;
+    public string Address { get; init; } = address;
+}
+
+
+
 public class Person(string name, int age)
 {
+    // Explicit Constructor 'should' invoke Primary Constructor
     public Person(string name, string country, int age) : this(name, 100)
     {
         Country = country;
@@ -10,6 +35,7 @@ public class Person(string name, int age)
     public int Age => age;
     public string Country { get; set; }
 
+    // Mutating Primary Constructor Variables
     public void AssignNameAndAge(string newName,int newAge)
     {
         name = newName;
@@ -21,5 +47,4 @@ public class Employee(string firstName, string lastName, int age) : Person($"{fi
 {
     public string Department { get; set; }
 }
-
 
