3688a50216f680450865ff3c6ddec4b5e9cc3e36
Anu Viswan <anu.viswan@gmail.com>
2024-05-13 21:13:35 +0530

    Working on BlobService

--- WebApi/FileUploadEndPoint/Services/IBlobService.cs
+++ WebApi/FileUploadEndPoint/Services/IBlobService.cs
@@ -1,4 +1,6 @@
-﻿namespace FileUploadEndPoint.Services;
+﻿using Azure.Storage.Blobs;
+
+namespace FileUploadEndPoint.Services;
 
 public interface IBlobService
 {
@@ -7,6 +9,18 @@ public interface IBlobService
 
 public class BlobService : IBlobService
 {
+    // TODO: Read from configuration file
+    private const string ConnectionString = "";
+    private const string ContainerName = "";
+
+    private readonly BlobServiceClient _blobServiceClient;
+    private readonly BlobContainerClient _bloblContainerClient;
+
+    public BlobService()
+    {
+        _blobServiceClient = new BlobServiceClient(ConnectionString);
+        _bloblContainerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+    }
     public Task UploadFile(IFormFile file)
     {
         throw new NotImplementedException();
