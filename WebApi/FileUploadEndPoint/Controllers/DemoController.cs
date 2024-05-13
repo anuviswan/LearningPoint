8e53ba6dce4e1f80b6c24dd7036f4ae9d0b6a518
Anu Viswan <a.viswan@kruss.de>
2024-05-13 16:25:30 +0530

    Adding democontroller

---
+++ WebApi/FileUploadEndPoint/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadEndPoint.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
