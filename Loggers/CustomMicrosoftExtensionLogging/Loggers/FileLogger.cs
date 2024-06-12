a07eab009fda7f33b37566dc3a4b31a59ccba354
Anu Viswan <anu.viswan@gmail.com>
2024-06-12 21:29:38 +0530

    adding example code for CustomMicrosoftExtensionlogging

---
+++ Loggers/CustomMicrosoftExtensionLogging/Loggers/FileLogger.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CustomMicrosoftExtensionLogging.Loggers;
+
+public class FileLogger : ILogger
+{
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        throw new NotImplementedException();
+    }
+}
