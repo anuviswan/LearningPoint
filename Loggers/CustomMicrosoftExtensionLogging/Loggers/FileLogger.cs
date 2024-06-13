d3a4223ffd5592bdf882f5389aeebedb01959d43
Anu Viswan <anu.viswan@gmail.com>
2024-06-13 08:08:08 +0530

    implementing isenabled

--- Loggers/CustomMicrosoftExtensionLogging/Loggers/FileLogger.cs
+++ Loggers/CustomMicrosoftExtensionLogging/Loggers/FileLogger.cs
@@ -1,17 +1,24 @@
 ﻿
+using CustomMicrosoftExtensionLogging.Settings;
+
 namespace CustomMicrosoftExtensionLogging.Loggers;
 
 public class FileLogger : ILogger
 {
+    private readonly FileLoggerConfiguration _configuration;
+    public FileLogger(FileLoggerConfiguration loggerConfiguration)
+    {
+        _configuration = loggerConfiguration;
+    }
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
 
-    public bool IsEnabled(LogLevel logLevel)
-    {
-        throw new NotImplementedException();
-    }
+    // Check minimum log levels
+    public bool IsEnabled(LogLevel logLevel) => logLevel >= _configuration.LogLevel;
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
+        if (!IsEnabled(logLevel)) return;
+
         throw new NotImplementedException();
     }
 }
