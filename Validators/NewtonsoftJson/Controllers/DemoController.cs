a3c89bfb8e2e8f57e8625a706096485ef12e4189
Anu Viswan <anu.viswan@gmail.com>
2024-06-23 22:02:12 +0530

    adding schema and middleware validator

--- Validators/NewtonsoftJson/Controllers/DemoController.cs
+++ Validators/NewtonsoftJson/Controllers/DemoController.cs
@@ -17,13 +17,7 @@ namespace NewtonsoftJson.Controllers
         [HttpPost(Name = "CreateUser")]
         public Task<IActionResult> CreateUser()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Validators/NewtonsoftJson/CustomMiddlewares/JsonValidator.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NewtonsoftJson.CustomMiddlewares
+{
+    public class JsonValidator
+    {
+        private readonly RequestDelegate _nextRequestDelegate;
+        private readonly JSchema _schema;
+        public JsonValidator(RequestDelegate next, string schemaPath)
+        {
+            (_nextRequestDelegate, _schema) = (next, JSchema.Parse(File.ReadAllText(schemaPath)));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(context.Request.Method == HttpMethods.Post)
+            {
+                context.Request.EnableBuffering();
+
+                using var reader = new StreamReader(context.Request.Body);
+
+                var body = await reader.ReadToEndAsync();
+                context.Request.Body.Position = 0;
+
+                var jsonBody = JObject.Parse(body);
+
+                if(jsonBody.IsValid(_schema, out IList<string> errors) is false)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid request: " + string.Join(", ", errors));
+                    return;
+                }
+            }
+
+            await _nextRequestDelegate(context);
+        }
+        
+    }
+}
