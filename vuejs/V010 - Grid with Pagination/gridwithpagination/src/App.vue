eef9ec034e2058bccf283ed38049a5b145cfb09a
Anu Viswan <a.viswan@kruss.de>
2024-09-26 16:11:36 +0530

    adding data

--- vuejs/V010 - Grid with Pagination/gridwithpagination/src/App.vue
+++ vuejs/V010 - Grid with Pagination/gridwithpagination/src/App.vue
@@ -1,85 +1,30 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <HomeView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  padding: 30px;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- vuejs/V010 - Grid with Pagination/gridwithpagination/src/views/HomeView.vue
+++ vuejs/V010 - Grid with Pagination/gridwithpagination/src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { reactive } from "vue";
+import axios from "axios";
+
+// Interface for each Pokémon result
+interface PokemonResult {
+  name: string; // Name of the Pokémon
+  url: string;  // URL to the Pokémon details
+}
+
+// Main interface for the response from the Pokémon API
+interface PokemonResponse {
+  count: number;                // Total number of Pokémon available
+  next: string | null;          // URL for the next set of results (if available)
+  previous: string | null;      // URL for the previous set of results (if available)
+  results: PokemonResult[];     // Array of Pokémon results
+}
+const gridItems = reactive<Array<PokemonResult>>([]);
+
+async function getData(){
+  console.log('loading')
+  var response = await axios.get<PokemonResponse>("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+ gridItems.value = response.data.results;
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+  <div class="container">
+    <header class="header">
+      <h1>My Vue App</h1>
+    </header>
+    <div class="button-container">
+      <button @click="getData()">Load Data</button>
+      <button >Button 2</button>
+      {{ gridItems.count }}
+    </div>
+    <div class="grid">
+      <div class="grid-item" v-for="item in gridItems.value" :key="item.id">
+        Name = {{ item.name }}
+      </div>
+    </div>
+  </div>
+  </template>
