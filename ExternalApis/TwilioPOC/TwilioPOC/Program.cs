2601928060286797cb0cb32ed0da6e262b105321
Anu Viswan <anu.viswan@gmail.com>
2025-04-07 21:16:53 +0530

    adding files

---
+++ ExternalApis/TwilioPOC/TwilioPOC/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/create", (string roomName) =>
+{
+    
+    return roomName;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
