517c512cb32544b5913737eb85eb89f76cb0aaac
Anu Viswan <anu.viswan@gmail.com>
2025-04-26 10:59:25 +0530

    C# 13 Feature explore

---
+++ FeatureExplore/CSharp13/CSharp13/ParamsCollection.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp13;
+
+internal class ParamsCollection
+{
+    private List<string> _list = [];
+    public void AddRange(params List<string> values)
+    {
+        _list.AddRange(values);
+    }
+
+    public void Print()
+    {
+        foreach (var item in _list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ FeatureExplore/CSharp13/CSharp13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * This Project demonstrates/explore C# 13 Language Features
+ *  1. params Collection
+ */
+
+using CSharp13;
+
+Console.WriteLine("Hello C# 13");
+
+var paramsCollection = new ParamsCollection();
+
+
+//paramsCollection.AddRange(new List<string> { "Red", "Blue", "Green" }); // Before C# 13
+//paramsCollection.AddRange(["Red", "Blue", "Green"]);   // Before C# 13
+
+paramsCollection.AddRange("Red", "Blue", "Green"); //Using C# 13, paramsCollection
+paramsCollection.Print();
