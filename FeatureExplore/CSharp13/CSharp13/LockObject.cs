e5a3386a5fdc389a86907ecbb825b8d6d5887492
Anu Viswan <anu.viswan@gmail.com>
2025-04-26 12:29:59 +0530

    Lock Object

---
+++ FeatureExplore/CSharp13/CSharp13/LockObject.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Concurrent;
+
+namespace CSharp13;
+
+internal class LockObject
+{
+    private int counter = 0;
+    private readonly object _traditionalLockObject = new();
+    private ConcurrentQueue<int> _queue = new();
+    public void DemoTraditionalApproach()
+    {
+        lock (_traditionalLockObject)
+        {
+            counter++;
+        }
+    }
+
+    private readonly Lock _ModernlockObject = new();
+    public void DemoModernApproach() 
+    {
+        lock (_ModernlockObject)
+        {
+            counter++;
+            _queue.Enqueue(counter);
+        }
+    }
+}
+
+[MemoryDiagnoser]
+public class BenchMarkLockObject
+{
+    [Params(1,3,5 ,10, 100, 1000)]
+    public int Counter { get; set; }
+
+    [Benchmark]
+    public void TraditionalApproach()
+    {
+        var lockObject = new LockObject();
+        Parallel.For(1, Counter,_ =>
+        {
+            lockObject.DemoTraditionalApproach();
+        });
+    }
+
+    [Benchmark]
+    public void ModernApproach()
+    {
+        var lockObject = new LockObject();
+        Parallel.For(1, Counter, _ =>
+        {
+            lockObject.DemoModernApproach();
+        });
+    }
+}
--- FeatureExplore/CSharp13/CSharp13/Program.cs
+++ FeatureExplore/CSharp13/CSharp13/Program.cs
@@ -3,6 +3,7 @@
  *  1. params Collection
  */
 
+using BenchmarkDotNet.Running;
 using CSharp13;
 
 Console.WriteLine("Hello C# 13");
@@ -15,3 +16,5 @@ var paramsCollection = new ParamsCollection();
 
 paramsCollection.AddRange("Red", "Blue", "Green"); //Using C# 13, paramsCollection
 paramsCollection.Print();
+
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
