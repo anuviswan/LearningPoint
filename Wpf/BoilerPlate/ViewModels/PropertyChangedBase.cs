aa377a0817fd61319b3949bdd6fc60fbe4b8572a
Anu Viswan <anu.viswan@gmail.com>
2025-04-26 09:36:11 +0530

    cleanup

---
+++ Wpf/BoilerPlate/ViewModels/PropertyChangedBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BoilerPlate.ViewModels
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyOnPropertyChanged([CallerMemberName] string? property = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
