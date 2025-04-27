bac8c1074b71032348573197e9ef72d33b2e61bf
Anu Viswan <anu.viswan@gmail.com>
2025-04-27 14:42:44 +0530

    added null conditional assignments

---
+++ FeatureExplore/CSharp14/NullConditionalAssignment.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp14;
+
+internal class NullConditionalAssignment
+{
+    public void Assign(int value)
+    {
+        var foo = new Foo();
+
+        /* 
+         * // C# 8.0 or earlier
+        if(foo is not null)
+        {
+            foo.Bar = value;
+        }
+        
+        */
+
+        foo?.Bar = 10;
+    }
+    private class Foo
+    {
+        public int Bar { get; set; }
+    }
+}
--- FeatureExplore/CSharp14/Program.cs
+++ FeatureExplore/CSharp14/Program.cs
@@ -2,12 +2,16 @@
 
 Console.WriteLine("Hello C# 14!");
 
+// Example of field backed properties
+var field = new Field();
+field.Foo = 10;
+
 var specialList = new SpecialList<int>();
 specialList.Add(1);
 specialList.Add(2);
 specialList.Add(4);
 
-// extension methods
+// extension members
 specialList.Insert(2,3);
 
 foreach(var items in specialList.Items)
@@ -17,3 +21,8 @@ foreach(var items in specialList.Items)
 
 Console.WriteLine($"Count: {specialList.Count}");
 Console.WriteLine($"IsEmpty: {SpecialList<int>.IsEmpty([1,2,3])}");
+
+
+// null conditional properties
+var nullConditionalAssignment = new NullConditionalAssignment();
+nullConditionalAssignment.Assign(10);
