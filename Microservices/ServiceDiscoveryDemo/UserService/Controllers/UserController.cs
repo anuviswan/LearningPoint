fbdee4f4ac050fdec310c2373eea872a53c0eef2
Anu Viswan <anu.viswan@gmail.com>
2025-05-13 21:36:47 +0530

    working on health check

--- Microservices/ServiceDiscoveryDemo/UserService/Controllers/UserController.cs
+++ Microservices/ServiceDiscoveryDemo/UserService/Controllers/UserController.cs
@@ -1,25 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace UserService.Controllers
+namespace UserService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class UserController : ControllerBase
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
     {
-        private readonly ILogger<UserController> _logger;
-
-        public UserController(ILogger<UserController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        [Route("GetUserInfo")]
-        public ActionResult<UserInfo> Get([FromQuery]string userName)
-        {
-            return Ok(new UserInfo("John Doe","1234456","john.doe@gmail.com"));
-        }
+        _logger = logger;
     }
 
-    public record UserInfo(string Name,string Phone,string Email);
+    [HttpGet]
+    [Route("GetUserInfo")]
+    public ActionResult<UserInfo> Get([FromQuery]string userName)
+    {
+        return Ok(new UserInfo("John Doe","1234456","john.doe@gmail.com"));
+    }
 }
+
+public record UserInfo(string Name,string Phone,string Email);
--- Microservices/ServiceDiscoveryDemo/UserService/Program.cs
+++ Microservices/ServiceDiscoveryDemo/UserService/Program.cs
@@ -1,10 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using Consul;
 
+var builder = WebApplication.CreateBuilder(args);
+var consulConfig = builder.Configuration.GetSection(nameof(ConsulConfig)).Get<ConsulConfig>();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+if(consulConfig is not null)
+{
+    var consulClient = new ConsulClient(x => x.Address = new Uri(consulConfig.ConsulAddress));
+    var registration = new AgentServiceRegistration
+    {
+        ID = consulConfig.ServiceId,
+        Name = consulConfig.ServiceName,
+        Address = consulConfig.ServiceAddress,
+        Port = consulConfig.ServicePort,
+        Check = new AgentServiceCheck
+        {
+            HTTP = $"http://{consulConfig.ServiceAddress}{consulConfig.HealthCheckUrl}",
+            Interval = TimeSpan.FromSeconds(10),
+            Timeout = TimeSpan.FromSeconds(5),
+            DeregisterCriticalServiceAfter = TimeSpan.FromMicroseconds(consulConfig.DeregisterAfterMinutes),
+        }
+    };
+
+
+    // Register service with Consul
+    await consulClient.Agent.ServiceRegister(registration);
+}
 
 var app = builder.Build();
 
@@ -21,3 +46,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public record ConsulConfig
+{
+    public string ConsulAddress { get; set; } = null!;
+    public string ServiceName { get; set; } = null!;
+    public string ServiceId { get; set; } = null!;
+    public string ServiceAddress { get; set; } = null!;
+    public int ServicePort { get; set; }
+    public string HealthCheckUrl { get; set; } = null!;
+    public int DeregisterAfterMinutes { get; set; }
+}
