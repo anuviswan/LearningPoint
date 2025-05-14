8a6cc57eb82a2da13bb1506ea93a869301057eb5
Anu Viswan <anu.viswan@gmail.com>
2025-05-14 19:28:42 +0530

    Added health check for payment service

---
+++ Microservices/ServiceDiscoveryDemo/PaymentService/Controllers/HeartBeatController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentService.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class HeartBeatController : ControllerBase
+{
+    private readonly ILogger<HeartBeatController> _logger;
+
+    public HeartBeatController(ILogger<HeartBeatController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [Route("health")]
+    public ActionResult Health()
+    {
+        return Ok();
+    }
+}
--- Microservices/ServiceDiscoveryDemo/PaymentService/Program.cs
+++ Microservices/ServiceDiscoveryDemo/PaymentService/Program.cs
@@ -1,10 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using Consul;
 
+var builder = WebApplication.CreateBuilder(args);
+var consulConfig = builder.Configuration.GetSection(nameof(ConsulConfig)).Get<ConsulConfig>();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+if (consulConfig is not null)
+{
+    var consulClient = new ConsulClient(x => x.Address = new Uri(consulConfig.ConsulAddress));
+    var registration = new AgentServiceRegistration
+    {
+        ID = consulConfig.ServiceId,
+        Name = consulConfig.ServiceName,
+        Address = consulConfig.ServiceAddress,
+        Port = consulConfig.ServicePort,
+        Check = new AgentServiceCheck
+        {
+            HTTP = $"https://{consulConfig.ServiceAddress}:{consulConfig.ServicePort}{consulConfig.HealthCheckUrl}",
+            Interval = TimeSpan.FromSeconds(10),
+            Timeout = TimeSpan.FromSeconds(5),
+            DeregisterCriticalServiceAfter = TimeSpan.FromMinutes(consulConfig.DeregisterAfterMinutes),
+            TLSSkipVerify = consulConfig.TLSSkipVerify,
+        }
+    };
+
+
+    // Register service with Consul
+    await consulClient.Agent.ServiceRegister(registration);
+}
 
 var app = builder.Build();
 
@@ -21,3 +47,15 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public record ConsulConfig
+{
+    public string ConsulAddress { get; set; } = null!;
+    public string ServiceName { get; set; } = null!;
+    public string ServiceId { get; set; } = null!;
+    public string ServiceAddress { get; set; } = null!;
+    public int ServicePort { get; set; }
+    public string HealthCheckUrl { get; set; } = null!;
+    public int DeregisterAfterMinutes { get; set; }
+    public bool TLSSkipVerify { get; set; } = true;
+}
