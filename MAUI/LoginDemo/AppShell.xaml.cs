eb7a60d4648a079074ddf4e4e5b9522346be11ea
Anu Viswan <anu.viswan@gmail.com>
2025-09-14 17:43:45 +0530

    adding login demo

---
+++ MAUI/LoginDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LoginDemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/LoginDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LoginDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
